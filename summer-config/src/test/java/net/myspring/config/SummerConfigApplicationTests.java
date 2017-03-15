f93fe87f737c70c4cbbc349067963e5fc940fc31
liuj <287724593@qq.com>
2017-03-15 17:32:39 +0800

    init config

---
+++ summer-config/src/test/java/net/myspring/config/SummerConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package net.myspring.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SummerConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
