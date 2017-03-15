86ccef73f0491609d6503619b824c88af44e8964
liuj <287724593@qq.com>
2017-03-15 18:36:24 +0800

    init common-cloud

---
+++ common-cloud/src/main/java/net/myspring/cloud/CommonCloudApplication.java
@@ -0,0 +1,12 @@
+package net.myspring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonCloudApplication.class, args);
+	}
+}
---
+++ common-cloud/src/test/java/net/myspring/cloud/CommonCloudApplicationTests.java
@@ -0,0 +1,16 @@
+package net.myspring.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommonCloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
