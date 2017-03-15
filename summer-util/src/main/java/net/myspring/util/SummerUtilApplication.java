b13c474e6e6664a22bc3b4b85fad15c7fcbc1f7e
liuj <287724593@qq.com>
2017-03-15 18:00:52 +0800

    init summer-util

---
+++ summer-util/src/main/java/net/myspring/util/SummerUtilApplication.java
@@ -0,0 +1,12 @@
+package net.myspring.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerUtilApplication.class, args);
+	}
+}
