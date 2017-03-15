d939a900f9f119c2276acc270e79ad850ab52280
liuj <287724593@qq.com>
2017-03-15 17:36:26 +0800

    init discovery

---
+++ summer-discovery/src/main/java/net/myspring/discovery/SummerDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package net.myspring.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerDiscoveryApplication.class, args);
+	}
+}
