f56c4663c90862e772dfe7005e727185779dce16
liuj <287724593@qq.com>
2017-03-15 18:21:05 +0800

    init hr

---
+++ summer-bean/src/main/java/net/myspring/bean/SummerBeanApplication.java
@@ -0,0 +1,12 @@
+package net.myspring.bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerBeanApplication.class, args);
+	}
+}
