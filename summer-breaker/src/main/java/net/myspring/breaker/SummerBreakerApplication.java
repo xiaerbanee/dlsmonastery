512aafb0d02ae9b9a56557c827c792e0db30d113
liuj <287724593@qq.com>
2017-03-15 17:40:57 +0800

    init breaker

---
+++ summer-breaker/src/main/java/net/myspring/breaker/SummerBreakerApplication.java
@@ -0,0 +1,12 @@
+package net.myspring.breaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerBreakerApplication.class, args);
+	}
+}
