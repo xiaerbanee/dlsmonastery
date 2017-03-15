704cfd107b95163b612cbd61d8b5a0d2c72d1986
liuj <287724593@qq.com>
2017-03-15 18:12:54 +0800

    init myspring-util and mybatis-smart

---
+++ mybatis-smart/src/main/java/net/myspring/mybatis/MybatisSmartApplication.java
@@ -0,0 +1,12 @@
+package net.myspring.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisSmartApplication.class, args);
+	}
+}
