91cc61b0a76f8ce59f57802b98f9777b837caebb
liuj <287724593@qq.com>
2017-03-16 10:08:31 +0800

    update gradle

--- myspring-utils/src/test/java/net/myspring/utils/MyspringUtilsApplicationTests.java
+++ myspring-utils/src/test/java/net/myspring/utils/MyspringUtilsApplicationTests.java
@@ -1,4 +1,4 @@
-package net.myspring.util;
+package net.myspring.utils;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MyspringUtilApplicationTests {
+public class MyspringUtilsApplicationTests {
 
 	@Test
 	public void contextLoads() {
