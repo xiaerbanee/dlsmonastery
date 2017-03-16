09f19535814f99a0ddd5d8082d99d62e5fe791d6
liuj <287724593@qq.com>
2017-03-16 10:09:14 +0800

    update gradle

---
+++ myspring-utils/src/main/java/net/myspring/utils/MyspringUtilsApplication.java
@@ -0,0 +1,12 @@
+package net.myspring.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringUtilsApplication.class, args);
+	}
+}
