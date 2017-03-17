4ab6fe7e132e52751968f82b4fa2b684c537bf59
liuj <287724593@qq.com>
2017-03-17 22:52:13 +0800

    add tableDto and columnDto

--- mybatis-smart/src/main/java/net/myspring/mybatis/dto/ColumnDto.java
+++ mybatis-smart/src/main/java/net/myspring/mybatis/dto/ColumnDto.java
@@ -1,7 +1,64 @@
 package net.myspring.mybatis.dto;
 
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.TypeHandler;
+
 /**
  * Created by liuj on 2017/3/17.
  */
 public class ColumnDto {
+    private TableDto tableDto;
+    private String javaInstance;
+    private String jdbcColumn;
+    private Boolean insertable;
+    private Boolean updatable;
+    private  Boolean nullable;
+
+    public TableDto getTableDto() {
+        return tableDto;
+    }
+
+    public void setTableDto(TableDto tableDto) {
+        this.tableDto = tableDto;
+    }
+
+    public String getJavaInstance() {
+        return javaInstance;
+    }
+
+    public void setJavaInstance(String javaInstance) {
+        this.javaInstance = javaInstance;
+    }
+
+    public String getJdbcColumn() {
+        return jdbcColumn;
+    }
+
+    public void setJdbcColumn(String jdbcColumn) {
+        this.jdbcColumn = jdbcColumn;
+    }
+
+    public Boolean getInsertable() {
+        return insertable;
+    }
+
+    public void setInsertable(Boolean insertable) {
+        this.insertable = insertable;
+    }
+
+    public Boolean getUpdatable() {
+        return updatable;
+    }
+
+    public void setUpdatable(Boolean updatable) {
+        this.updatable = updatable;
+    }
+
+    public Boolean getNullable() {
+        return nullable;
+    }
+
+    public void setNullable(Boolean nullable) {
+        this.nullable = nullable;
+    }
 }
--- mybatis-smart/src/main/java/net/myspring/mybatis/dto/TableDto.java
+++ mybatis-smart/src/main/java/net/myspring/mybatis/dto/TableDto.java
@@ -1,7 +1,111 @@
 package net.myspring.mybatis.dto;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by liuj on 2017/3/17.
  */
 public class TableDto {
+    private String jdbcTable;
+    private List<ColumnDto> mybatisColumnList;
+    private ColumnDto idColumn;
+    private ColumnDto createdByColumn;
+    private ColumnDto createdDateColumn;
+    private ColumnDto lastModifiedByColumn;
+    private ColumnDto LastModifiedDateColumn;
+    private ColumnDto versionColumn;
+    private ColumnDto enabledColumn;
+    private String generationType;
+    private Map<String,ColumnDto> columnMap;
+
+    public String getJdbcTable() {
+        return jdbcTable;
+    }
+
+    public void setJdbcTable(String jdbcTable) {
+        this.jdbcTable = jdbcTable;
+    }
+
+    public List<ColumnDto> getMybatisColumnList() {
+        return mybatisColumnList;
+    }
+
+    public void setMybatisColumnList(List<ColumnDto> mybatisColumnList) {
+        this.mybatisColumnList = mybatisColumnList;
+    }
+
+    public ColumnDto getIdColumn() {
+        return idColumn;
+    }
+
+    public void setIdColumn(ColumnDto idColumn) {
+        this.idColumn = idColumn;
+    }
+
+    public ColumnDto getCreatedByColumn() {
+        return createdByColumn;
+    }
+
+    public void setCreatedByColumn(ColumnDto createdByColumn) {
+        this.createdByColumn = createdByColumn;
+    }
+
+    public ColumnDto getCreatedDateColumn() {
+        return createdDateColumn;
+    }
+
+    public void setCreatedDateColumn(ColumnDto createdDateColumn) {
+        this.createdDateColumn = createdDateColumn;
+    }
+
+    public ColumnDto getLastModifiedByColumn() {
+        return lastModifiedByColumn;
+    }
+
+    public void setLastModifiedByColumn(ColumnDto lastModifiedByColumn) {
+        this.lastModifiedByColumn = lastModifiedByColumn;
+    }
+
+    public ColumnDto getLastModifiedDateColumn() {
+        return LastModifiedDateColumn;
+    }
+
+    public void setLastModifiedDateColumn(ColumnDto lastModifiedDateColumn) {
+        LastModifiedDateColumn = lastModifiedDateColumn;
+    }
+
+    public ColumnDto getVersionColumn() {
+        return versionColumn;
+    }
+
+    public void setVersionColumn(ColumnDto versionColumn) {
+        this.versionColumn = versionColumn;
+    }
+
+    public ColumnDto getEnabledColumn() {
+        return enabledColumn;
+    }
+
+    public void setEnabledColumn(ColumnDto enabledColumn) {
+        this.enabledColumn = enabledColumn;
+    }
+
+    public String getGenerationType() {
+        return generationType;
+    }
+
+    public void setGenerationType(String generationType) {
+        this.generationType = generationType;
+    }
+
+    public Map<String, ColumnDto> getColumnMap() {
+        return columnMap;
+    }
+
+    public void setColumnMap(Map<String, ColumnDto> columnMap) {
+        this.columnMap = columnMap;
+    }
 }
