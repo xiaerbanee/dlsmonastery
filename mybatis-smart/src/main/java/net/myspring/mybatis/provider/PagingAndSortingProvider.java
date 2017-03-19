b60f6805618a500a210725bf919e44e7c0007405
liuj <287724593@qq.com>
2017-03-19 13:32:03 +0800

    add tableDto and columnDto

--- mybatis-smart/src/main/java/net/myspring/mybatis/provider/PagingAndSortingProvider.java
+++ mybatis-smart/src/main/java/net/myspring/mybatis/provider/PagingAndSortingProvider.java
@@ -3,16 +3,31 @@ package net.myspring.mybatis.provider;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.util.Iterator;
+
 /**
  * Created by liuj on 2017/3/18.
  */
 public class PagingAndSortingProvider extends CrudProvider {
 
     public String findAllBySort(Sort sort) {
-        return "";
+        StringBuilder sb = new StringBuilder("SELECT * FROM " + getTableDto().getJdbcTable());
+        if(sort != null) {
+            sb.append(" ORDER BY ");
+            Iterator<Sort.Order> iterator = sort.iterator();
+            while (iterator.hasNext()) {
+                Sort.Order order = iterator.next();
+                sb.append(order.getProperty()).append(" ").append(order.getDirection());
+                if(iterator.hasNext()) {
+                    sb.append(", ");
+                }
+            }
+        }
+        return sb.toString();
     }
 
     public String findAllByPageable(Pageable pageable) {
-        return "";
+        String sql = "SELECT * FROM " + getTableDto().getJdbcTable();
+        return sql;
     }
 }
