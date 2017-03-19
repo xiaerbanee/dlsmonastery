756166dee9d67d4be924fca190e9508ce1633153
liuj <287724593@qq.com>
2017-03-19 21:26:22 +0800

    add tableDto and columnDto

--- common-hr/src/main/java/net/myspring/hr/config/MybatisConfig.java
+++ common-hr/src/main/java/net/myspring/hr/config/MybatisConfig.java
@@ -3,7 +3,6 @@ package net.myspring.hr.config;
 import net.myspring.mybatis.context.MybatisContext;
 import net.myspring.mybatis.dialect.Dialect;
 import net.myspring.mybatis.dialect.MySQLDialect;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
