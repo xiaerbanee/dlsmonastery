726f0b1964e196918cad2963a0659a698464845d
liuj <287724593@qq.com>
2017-03-27 21:31:08 +0800

    全部注册为服务

--- summer-gateway/src/main/java/net/myspring/gateway/SummerGatewayApplication.java
+++ summer-gateway/src/main/java/net/myspring/gateway/SummerGatewayApplication.java
@@ -1,8 +1,10 @@
 package net.myspring.gateway;
 
+import net.myspring.gateway.fiter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +12,10 @@ public class SummerGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SummerGatewayApplication.class, args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
 	}
 }
---
+++ summer-gateway/src/main/java/net/myspring/gateway/fiter/AccessFilter.java
@@ -0,0 +1,46 @@
+package net.myspring.gateway.fiter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by liuj on 2017/3/27.
+ */
+public class AccessFilter extends ZuulFilter {
+    private static Logger logger = LoggerFactory.getLogger(AccessFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        logger.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        Object accessToken = request.getParameter("accessToken");
+        if(accessToken == null) {
+            logger.warn("access token is empty");
+            return null;
+        }
+        logger.info("access token ok");
+        return null;
+    }
+}
