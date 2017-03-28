ff3c8501739a0b13de0b574e88f1b428c48bfe17
liuj <287724593@qq.com>
2017-03-28 13:33:56 +0800

    update

--- summer-gateway/src/main/java/net/myspring/gateway/SummerGatewayApplication.java
+++ summer-gateway/src/main/java/net/myspring/gateway/SummerGatewayApplication.java
@@ -3,12 +3,16 @@ package net.myspring.gateway;
 import net.myspring.gateway.fiter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableResourceServer
+@EnableOAuth2Sso
 @EnableZuulProxy
 public class SummerGatewayApplication {
 
