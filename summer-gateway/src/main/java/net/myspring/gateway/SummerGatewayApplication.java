19ef005d0960e32a19f5ad88ab2121490790aba5
liuj <287724593@qq.com>
2017-03-28 23:43:46 +0800

    網關可以直接訪問uaa

--- summer-gateway/src/main/java/net/myspring/gateway/SummerGatewayApplication.java
+++ summer-gateway/src/main/java/net/myspring/gateway/SummerGatewayApplication.java
@@ -3,7 +3,6 @@ package net.myspring.gateway;
 import net.myspring.gateway.fiter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +10,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableOAuth2Sso
+@EnableResourceServer
 @EnableZuulProxy
 public class SummerGatewayApplication {
 
--- summer-gateway/src/main/java/net/myspring/gateway/config/ResourceServerConfig.java
+++ summer-gateway/src/main/java/net/myspring/gateway/config/ResourceServerConfig.java
@@ -1,16 +1,9 @@
 package net.myspring.gateway.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by on 28.01.16.
