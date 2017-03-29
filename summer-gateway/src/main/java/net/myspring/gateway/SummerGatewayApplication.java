4a6ea48316a2869e75a7d34869367cd0d718aa8b
liuj <287724593@qq.com>
2017-03-29 10:38:47 +0800

    服务间调用

--- summer-gateway/src/main/java/net/myspring/gateway/SummerGatewayApplication.java
+++ summer-gateway/src/main/java/net/myspring/gateway/SummerGatewayApplication.java
@@ -10,7 +10,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableResourceServer
 @EnableZuulProxy
 public class SummerGatewayApplication {
 
