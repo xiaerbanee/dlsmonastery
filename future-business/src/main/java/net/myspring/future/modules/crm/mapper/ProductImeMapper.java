7c0e08daedbd855ec81de557e1a81379be3ca1ce
liuj <287724593@qq.com>
2017-04-02 16:43:02 +0800

    future-business 模塊

--- future-business/src/main/java/net/myspring/future/modules/crm/mapper/ProductImeMapper.java
+++ future-business/src/main/java/net/myspring/future/modules/crm/mapper/ProductImeMapper.java
@@ -3,11 +3,8 @@ package net.myspring.future.modules.crm.mapper;
 import net.myspring.future.modules.crm.domain.ProductIme;
 import net.myspring.mybatis.mapper.CrudMapper;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.Map;
 
 /**
  * Created by liuj on 2017/4/2.
--- future-business/src/main/java/net/myspring/future/modules/crm/service/ProductImeService.java
+++ future-business/src/main/java/net/myspring/future/modules/crm/service/ProductImeService.java
@@ -6,10 +6,12 @@ import net.myspring.future.modules.crm.mapper.ProductImeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by liuj on 2017/4/2.
  */
+@Service
 public class ProductImeService {
 
         @Autowired
