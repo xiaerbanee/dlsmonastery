26bba86bbbd9d69c62a87a86185c9b6cf0703a59
liuj <287724593@qq.com>
2017-04-02 09:28:52 +0800

    uaa与数据库关联

--- summer-uaa/src/main/java/net/myspring/uaa/security/CustomUserDetails.java
+++ summer-uaa/src/main/java/net/myspring/uaa/security/CustomUserDetails.java
@@ -1,5 +1,6 @@
 package net.myspring.uaa.security;
 
+import com.google.common.collect.Sets;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.SpringSecurityCoreVersion;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -33,7 +34,7 @@ public class CustomUserDetails implements UserDetails {
         this.accountNonExpired = accountNonExpired;
         this.credentialsNonExpired = credentialsNonExpired;
         this.accountNonLocked = accountNonLocked;
-        this.authorities = Collections.unmodifiableSet(sortAuthorities(authorities));
+        this.authorities = Sets.newHashSet(authorities);
         this.companyId = companyId;
         this.positionId = positionId;
     }
@@ -80,36 +81,5 @@ public class CustomUserDetails implements UserDetails {
 
     public String getPositionId() {
         return positionId;
-    }
-
-    private static SortedSet<GrantedAuthority> sortAuthorities(Collection<? extends GrantedAuthority> authorities) {
-        Assert.notNull(authorities,"Cannot pass a null GrantedAuthority collection");
-        // Ensure array iteration order is predictable (as per
-        // UserDetails.getAuthorities() contract and SEC-717)
-        SortedSet<GrantedAuthority> sortedAuthorities = new TreeSet<GrantedAuthority>(new AuthorityComparator());
-        for (GrantedAuthority grantedAuthority : authorities) {
-            Assert.notNull(grantedAuthority,"GrantedAuthority list cannot contain any null elements");
-            sortedAuthorities.add(grantedAuthority);
-        }
-        return sortedAuthorities;
-    }
-
-    private static class AuthorityComparator implements Comparator<GrantedAuthority>, Serializable {
-        private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
-        public int compare(GrantedAuthority g1, GrantedAuthority g2) {
-            // Neither should ever be null as each entry is checked before
-            // adding it to
-            // the set.
-            // If the authority is null, it is a custom authority and should
-            // precede
-            // others.
-            if (g2.getAuthority() == null) {
-                return -1;
-            }else if (g1.getAuthority() == null) {
-                return 1;
-            } else {
-                return g1.getAuthority().compareTo(g2.getAuthority());
-            }
-        }
     }
 }
