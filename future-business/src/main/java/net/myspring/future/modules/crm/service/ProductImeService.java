d9f3bcdc68134c08cc3a13fca683b836ccd85ba0
liuj <287724593@qq.com>
2017-04-03 13:16:26 +0800

    es client

--- future-business/src/main/java/net/myspring/future/modules/crm/service/ProductImeService.java
+++ future-business/src/main/java/net/myspring/future/modules/crm/service/ProductImeService.java
@@ -3,10 +3,18 @@ package net.myspring.future.modules.crm.service;
 import net.myspring.future.modules.crm.domain.ProductIme;
 import net.myspring.future.modules.crm.manager.ProductImeManager;
 import net.myspring.future.modules.crm.mapper.ProductImeMapper;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * Created by liuj on 2017/4/2.
@@ -17,7 +25,17 @@ public class ProductImeService {
         @Autowired
         private ProductImeManager productImeManager;
 
-        public Page<ProductIme> findPage(Pageable pageable) {
-            return productImeManager.findPage(pageable);
+        public Page<ProductIme> findPage(Pageable pageable)  {
+            TransportClient client = null;
+            try {
+                client = new PreBuiltXPackTransportClient(Settings.builder().put("xpack.security.user", "elastic:changeme")
+                        .build()).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+
+            GetResponse response = client.prepareGet("megacorp", "employee", "1").execute().actionGet();
+            System.out.println(response.getSourceAsString());
+            return null;
         }
 }
