d5c6ad0ae43ac4a77b531ec4bf047e043dcd1e17
liuj <287724593@qq.com>
2017-04-05 17:12:01 +0800

    增加global-tool

---
+++ mybatis-smart/src/main/java/net/myspring/mybatis/dialect/SQLServerDialect.java
@@ -0,0 +1,33 @@
+package net.myspring.mybatis.dialect;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.Iterator;
+
+/**
+ * Created by liuj on 2017/4/5.
+ */
+public class SQLServerDialect extends Dialect {
+    @Override
+    public String getPageableSql(String sql, Pageable pageable) {
+        StringBuilder sb = new StringBuilder(sql);
+        if(pageable.getSort() != null) {
+            sb.append(" ORDER BY ");
+            Iterator<Sort.Order> iterator = pageable.getSort().iterator();
+            while (iterator.hasNext()) {
+                Sort.Order order = iterator.next();
+                sb.append(order.getProperty()).append(" ").append(order.getDirection());
+                if(iterator.hasNext()) {
+                    sb.append(", ");
+                }
+            }
+        }
+        if (pageable.getOffset() == 0) {
+            sb.append(" LIMIT ").append(pageable.getPageSize());
+        } else {
+            sb.append(" LIMIT ").append(" ").append(pageable.getOffset()).append(",").append(pageable.getPageSize());
+        }
+        return sb.toString();
+    }
+}
