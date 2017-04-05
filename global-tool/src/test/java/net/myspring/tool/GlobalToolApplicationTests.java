04492a10c30ef6bffcd347665c6957639e31e445
liuj <287724593@qq.com>
2017-04-05 15:16:51 +0800

    增加global-tool

---
+++ global-tool/src/test/java/net/myspring/tool/GlobalToolApplicationTests.java
@@ -0,0 +1,16 @@
+package net.myspring.tool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GlobalToolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
