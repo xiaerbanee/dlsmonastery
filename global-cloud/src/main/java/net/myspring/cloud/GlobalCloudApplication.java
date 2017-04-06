d9e881dfaca2396ba55586c84b3f483651fef7b3
liuj <287724593@qq.com>
2017-04-06 18:06:48 +0800

    消息队列

--- global-cloud/src/main/java/net/myspring/cloud/GlobalCloudApplication.java
+++ global-cloud/src/main/java/net/myspring/cloud/GlobalCloudApplication.java
@@ -3,14 +3,23 @@ package net.myspring.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
 @EnableDiscoveryClient
+@EnableBinding(Sink.class)
 public class GlobalCloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlobalCloudApplication.class, args);
+	}
+
+	@StreamListener(Sink.INPUT)
+	public void log(String message) {
+		System.out.println(message);
 	}
 }
--- global-tool/src/main/java/net/myspring/tool/GlobalToolApplication.java
+++ global-tool/src/main/java/net/myspring/tool/GlobalToolApplication.java
@@ -3,6 +3,8 @@ package net.myspring.tool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
