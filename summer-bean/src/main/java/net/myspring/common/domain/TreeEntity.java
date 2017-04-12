3d0ab9d8b22f56f45a1b7e27ecb9be6345c8e0b7
liuj <287724593@qq.com>
2017-04-12 14:39:50 +0800

    增加companyEntity

--- summer-bean/src/main/java/net/myspring/common/domain/TreeEntity.java
+++ summer-bean/src/main/java/net/myspring/common/domain/TreeEntity.java
@@ -9,7 +9,7 @@ import java.util.List;
 /**
  * Created by liuj on 2016-07-25.
  */
-public class TreeEntity<T> extends DataEntity<T> {
+public class TreeEntity<T> extends CompanyEntity<T> {
     private String parentId;
     private String parentIds;
 
