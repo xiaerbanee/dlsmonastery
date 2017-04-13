2182fba880d31f1c961565fc0ea40739593e408a
liuj <287724593@qq.com>
2017-04-13 13:54:18 +0800

    mybatis-smart update

--- global-tool/src/main/java/net/myspring/tool/common/config/MybatisContextImpl.java
+++ global-tool/src/main/java/net/myspring/tool/common/config/MybatisContextImpl.java
@@ -6,17 +6,27 @@ import net.myspring.mybatis.dialect.MySQLDialect;
 import net.myspring.tool.common.dataSource.DynamicDataSourceContext;
 import net.myspring.tool.common.enums.DataSourceTypeEnum;
 import net.myspring.tool.common.utils.SecurityUtils;
+import net.myspring.util.json.ObjectMapperUtils;
+import org.apache.commons.lang.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+
+import java.util.Map;
 
 /**
  * Created by liuj on 2017/3/21.
  */
 public class MybatisContextImpl implements MybatisContext {
-    @Autowired
-    private SecurityUtils securityUtils;
+
     @Override
     public String getAccountId() {
-        return securityUtils.getAccountId();
+        final OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getDetails();
+        Jwt jwt = JwtHelper.decode(details.getTokenValue());
+        Map<String,Object> map = ObjectMapperUtils.readValue(jwt.getClaims(),Map.class);
+        return ObjectUtils.toString(map.get("accountId"));
     }
 
     @Override
--- summer-basic/src/main/java/net/myspring/basic/common/config/MybatisContextImpl.java
+++ summer-basic/src/main/java/net/myspring/basic/common/config/MybatisContextImpl.java
@@ -5,9 +5,18 @@ import net.myspring.basic.common.utils.SecurityUtils;
 import net.myspring.mybatis.context.MybatisContext;
 import net.myspring.mybatis.dialect.Dialect;
 import net.myspring.mybatis.dialect.MySQLDialect;
+import net.myspring.util.json.ObjectMapperUtils;
+import net.myspring.util.text.StringUtils;
+import org.apache.commons.lang.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * Created by liuj on 2017/3/21.
@@ -16,8 +25,10 @@ public class MybatisContextImpl implements MybatisContext {
 
     @Override
     public String getAccountId() {
-        SecurityUtils securityUtils = SummerBasicApplication.getApplicationContext().getBean(SecurityUtils.class);
-        return securityUtils.getAccountId();
+        final OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getDetails();
+        Jwt jwt = JwtHelper.decode(details.getTokenValue());
+        Map<String,Object> map = ObjectMapperUtils.readValue(jwt.getClaims(),Map.class);
+        return ObjectUtils.toString(map.get("accountId"));
     }
 
     @Override
