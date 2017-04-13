2989d28425239d377c309c18d5b8ce2ab44c03d7
liuj <287724593@qq.com>
2017-04-13 14:22:45 +0800

    去除saveForm

--- summer-basic/src/main/java/net/myspring/basic/SummerBasicApplication.java
+++ summer-basic/src/main/java/net/myspring/basic/SummerBasicApplication.java
@@ -3,21 +3,14 @@ package net.myspring.basic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ApplicationContext;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
 @EnableDiscoveryClient
 public class SummerBasicApplication {
-	private static ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
-		final ApplicationContext applicationContext = SpringApplication.run(SummerBasicApplication.class, args);
-		SummerBasicApplication.applicationContext = applicationContext;
-	}
-
-	public static ApplicationContext getApplicationContext() {
-		return applicationContext;
+		SpringApplication.run(SummerBasicApplication.class, args);
 	}
 }
