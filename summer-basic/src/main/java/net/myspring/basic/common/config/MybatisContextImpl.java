e151a5ce2469a889d1316aa66323e374b9107cf1
liuj <287724593@qq.com>
2017-04-13 13:00:29 +0800

    mybatis-smart update

--- summer-basic/src/main/java/net/myspring/basic/common/config/MybatisContextImpl.java
+++ summer-basic/src/main/java/net/myspring/basic/common/config/MybatisContextImpl.java
@@ -1,20 +1,22 @@
 package net.myspring.basic.common.config;
 
+import net.myspring.basic.SummerBasicApplication;
 import net.myspring.basic.common.utils.SecurityUtils;
 import net.myspring.mybatis.context.MybatisContext;
 import net.myspring.mybatis.dialect.Dialect;
 import net.myspring.mybatis.dialect.MySQLDialect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by liuj on 2017/3/21.
  */
 public class MybatisContextImpl implements MybatisContext {
-    @Autowired
-    private SecurityUtils securityUtils;
 
     @Override
     public String getAccountId() {
+        SecurityUtils securityUtils = SummerBasicApplication.getApplicationContext().getBean(SecurityUtils.class);
         return securityUtils.getAccountId();
     }
 
