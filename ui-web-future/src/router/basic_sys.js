39a8ff95b7d172a3448c4b664e9d32bb5ff56941
liuj <287724593@qq.com>
2017-04-18 17:11:02 +0800

    update

--- ui-web-future/src/router/basic_sys.js
+++ ui-web-future/src/router/basic_sys.js
@@ -21,8 +21,8 @@ const processList= r => require.ensure([], () => r(require('pages/basic/sys/proc
 
 let routes = [
   {path:'/basic/sys/processList',component:processList,name:'processList'},
-  {path:'/basic/sys/dictEnumList',component:dictEnumList,name:'dictEnumList'},
-  {path:'/basic/sys/dictEnumForm',component:dictEnumForm,name:'dictEnumForm',meta: {menu:"dictEnumList"}},
+  {path:'/basic/sys/dictEnumList',component:dictEnumList,name:'dictEnumList',meta: {menu:"dictEnumList"}},
+  {path:'/basic/sys/dictEnumForm',component:dictEnumForm,name:'dictEnumForm',meta: {menu:"dictEnumForm"}},
   {path:'/basic/sys/dictMapList',component:dictMapList,name:'dictMapList'},
   {path:'/basic/sys/dictMapForm',component:dictMapForm,name:'dictMapForm',meta: {menu:"dictMapList"}},
   {path:'/basic/sys/companyConfigList',component:companyConfigList,name:'companyConfigList'},
