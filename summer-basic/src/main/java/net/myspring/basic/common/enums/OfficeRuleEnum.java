5fb286becabd4bcc5fd88dc6aec81e42be4ff6e8
zhucc <1125517699@qq.com>
2017-04-26 10:12:34 +0800

    搜索

--- summer-basic/src/main/java/net/myspring/basic/common/enums/OfficeRuleEnum.java
+++ summer-basic/src/main/java/net/myspring/basic/common/enums/OfficeRuleEnum.java
@@ -12,19 +12,54 @@ import java.util.Map;
  * Created by wangzm on 2017/4/24.
  */
 public enum OfficeRuleEnum {
-    //业务部门
-    business,
-    //后勤部门
-    support;
+    业务部门("businessOffice",100),
+    后勤部门("rearOffice",200);
 
-    private static List<String> list= Lists.newArrayList();
+    private static List<OfficeRuleEnum> officeRuleEnumList= Lists.newArrayList();
+    private static Map<Integer,String> map= Maps.newHashMap();
+    private String code;
+    private Integer type;
 
-    public static List<String> getList() {
-        if(CollectionUtil.isEmpty(list)){
-            for(OfficeRuleEnum officeRuleEnum:OfficeRuleEnum.values()) {
-                list.add(officeRuleEnum.name());
+    OfficeRuleEnum(String code,Integer type){
+        this.type=type;
+        this.code=code;
+    }
+
+    public static List<OfficeRuleEnum> getOfficeRuleEnumList(){
+        if(CollectionUtil.isEmpty(officeRuleEnumList)){
+            for(OfficeRuleEnum officeRuleEnum:OfficeRuleEnum.values()){
+                officeRuleEnumList.add(officeRuleEnum);
             }
         }
-        return list;
+        return officeRuleEnumList;
+    }
+
+    public static Map<Integer,String> getMap(){
+        if(CollectionUtil.isEmpty(map)){
+            for(OfficeRuleEnum officeRuleEnum:OfficeRuleEnum.values()){
+                map.put(officeRuleEnum.type,officeRuleEnum.name());
+            }
+        }
+        return map;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getValue(){
+        return name();
     }
 }
