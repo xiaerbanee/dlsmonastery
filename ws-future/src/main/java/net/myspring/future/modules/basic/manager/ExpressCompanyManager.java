7142ff2b85f2957c490a59c02e0c27e05ee94e8b
liuj <287724593@qq.com>
2017-04-28 10:34:49 +0800

    update

--- ws-future/src/main/java/net/myspring/future/modules/basic/manager/ExpressCompanyManager.java
+++ ws-future/src/main/java/net/myspring/future/modules/basic/manager/ExpressCompanyManager.java
@@ -4,6 +4,7 @@ import net.myspring.future.modules.basic.domain.ExpressCompany;
 import net.myspring.future.modules.basic.mapper.ExpressCompanyMapper;
 import net.myspring.future.modules.basic.web.form.ExpressCompanyForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -15,34 +16,35 @@ import java.util.List;
  * Created by lihx on 2017/4/20.
  */
 @Component
+@CacheConfig(cacheNames = "expressCompanies")
 public class ExpressCompanyManager {
     @Autowired
     private ExpressCompanyMapper expressCompanyMapper;
 
-    @Cacheable(value = "expressCompanies",key="#p0")
+    @Cacheable(key="#p0")
     public ExpressCompany findOne(String id) {
         return expressCompanyMapper.findOne(id);
     }
 
-    @CachePut(value = "expressCompanys",key="#p0.id")
+    @CachePut(key="#p0.id")
     public ExpressCompany save(ExpressCompany expressCompany){
         expressCompanyMapper.save(expressCompany);
         return  expressCompany;
     }
 
-    @CachePut(value = "expressCompanys",key="#p0.id")
+    @CachePut(key="#p0.id")
     public ExpressCompany update(ExpressCompany expressCompany){
         expressCompanyMapper.update(expressCompany);
         return  expressCompanyMapper.findOne(expressCompany.getId());
     }
 
-    @CachePut(value = "expressCompanys",key="#p0.id")
+    @CachePut(key="#p0.id")
     public ExpressCompany updateForm(ExpressCompanyForm expressCompanyForm){
         expressCompanyMapper.updateForm(expressCompanyForm);
         return  expressCompanyMapper.findOne(expressCompanyForm.getId());
     }
 
-    @CacheEvict(value = "expressCompanys",key="#p0")
+    @CacheEvict(key="#p0")
     public int deleteById(String id) {
         return expressCompanyMapper.deleteById(id);
     }
