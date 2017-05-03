36bcd03d216fe48439d7796b8d91afaae38ef2c8
zhucc <1125517699@qq.com>
2017-05-03 09:42:32 +0800

    update

--- ws-future/src/main/java/net/myspring/future/modules/layout/web/controller/ShopImageController.java
+++ ws-future/src/main/java/net/myspring/future/modules/layout/web/controller/ShopImageController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = "crm/shopImage")
+@RequestMapping(value = "layout/shopImage")
 public class ShopImageController {
 
 
