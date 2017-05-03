55025fea98538ab7be42e37deca18dcdfccb7b0c
lihx <lixh@qq.com>
2017-05-03 12:09:50 +0800

    update

---
+++ global-cloud/src/main/java/net/myspring/cloud/modules/report/dto/NameValueDto.java
@@ -0,0 +1,25 @@
+package net.myspring.cloud.modules.report.dto;
+
+/**
+ * Created by lihx on 2017/5/2.
+ */
+public class NameValueDto {
+    private String name;
+    private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
