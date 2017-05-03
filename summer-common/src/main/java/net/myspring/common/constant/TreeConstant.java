dbcc6f123c43948ddbda567acae674031768977b
wangzhangmao <wangcyh194520>
2017-05-03 17:08:57 +0800

    basic

--- summer-common/src/main/java/net/myspring/common/constant/TreeConstant.java
+++ summer-common/src/main/java/net/myspring/common/constant/TreeConstant.java
@@ -4,7 +4,7 @@ package net.myspring.common.constant;
  * Created by liuj on 2017/5/3.
  */
 public class TreeConstant {
-    public static final String ROOT_ID = "0";
 
+    public static final String ROOT_ID = "0";
     public static final String ROOT_PARENT_IDS = "0,";
 }
