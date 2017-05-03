322215d7e441da9c57817eee2beb83d5b2c60653
liuj <287724593@qq.com>
2017-05-03 16:55:55 +0800

    update

---
+++ summer-common/src/main/java/net/myspring/common/constant/CharConstant.java
@@ -0,0 +1,13 @@
+package net.myspring.common.constant;
+
+/**
+ * Created by liuj on 2017/5/3.
+ */
+public class CharConstant {
+    public static final String SPACE_FULL = "　";
+    public static final String SPACE = " ";
+    public static final String COMMA = ",";
+    public static final String SLASH_LINE = "/";
+    public static final String ENTER = "\n";
+    public static final String COLON = ":";
+}
