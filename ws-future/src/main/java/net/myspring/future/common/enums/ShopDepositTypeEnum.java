610e093253e203d994dd0de3a1434ae468517bee
qianl <1141185821@qq.com>
2017-05-04 11:35:27 +0800

    update

--- ws-future/src/main/java/net/myspring/future/common/enums/ShopDepositTypeEnum.java
+++ ws-future/src/main/java/net/myspring/future/common/enums/ShopDepositTypeEnum.java
@@ -1,5 +1,10 @@
 package net.myspring.future.common.enums;
 
+import com.google.common.collect.Lists;
+import net.myspring.util.collection.CollectionUtil;
+
+import java.util.List;
+
 /**
  * Created by admin on 2017/2/20.
  */
@@ -7,4 +12,17 @@ public enum ShopDepositTypeEnum {
     市场保证金,
     形象保证金,
     演示机押金;
+
+    private static List<String> list= Lists.newArrayList();
+
+    public static List<String> getList(){
+        if(CollectionUtil.isEmpty(list)){
+            for(ShopDepositTypeEnum shopDepositTypeEnum:ShopDepositTypeEnum.values()){
+                list.add(shopDepositTypeEnum.name());
+            }
+        }
+        return list;
+    }
+
+
 }
--- ws-future/src/main/java/net/myspring/future/modules/layout/web/controller/ShopDepositController.java
+++ ws-future/src/main/java/net/myspring/future/modules/layout/web/controller/ShopDepositController.java
@@ -1,20 +1,32 @@
 package net.myspring.future.modules.layout.web.controller;
 
 
-import org.springframework.web.bind.annotation.*;
+import net.myspring.common.response.ResponseCodeEnum;
+import net.myspring.common.response.RestResponse;
+import net.myspring.future.modules.basic.dto.ShopDepositDto;
+import net.myspring.future.modules.layout.service.ShopDepositService;
+import net.myspring.future.modules.layout.web.form.ShopDepositForm;
+import net.myspring.future.modules.layout.web.query.ShopDepositQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "crm/shopDeposit")
 public class ShopDepositController {
 
-
     @RequestMapping(method = RequestMethod.GET)
-    public String list() {
-        return null;
+    public Page<ShopDepositDto> list(Pageable pageable, ShopDepositQuery shopDepositQuery){
+        Page<ShopDepositDto> page = shopDepositService.findPage(pageable, shopDepositQuery);
+        return page;
     }
+
 
     @RequestMapping(value = "getDepartmentByShopId")
     public String getDepartmentByShopId() {
@@ -27,27 +39,36 @@ public class ShopDepositController {
     }
 
     @RequestMapping(value = "getQuery")
-    public String getQuery() {
-        return null;
+    public ShopDepositQuery getQuery(ShopDepositQuery shopDepositQuery) {
+        shopDepositQuery.setTypeList(shopDepositService.findShopDepositTypeList());
+        return shopDepositQuery;
     }
 
     @RequestMapping(value = "delete")
-    public String delete() {
-        return null;
+    public RestResponse delete(ShopDepositForm shopDepositForm) {
+        shopDepositService.delete(shopDepositForm);
+        RestResponse restResponse=new RestResponse("删除成功", ResponseCodeEnum.removed.name());
+        return restResponse;
     }
 
+    /**
+     * 押金列表只能够新增，不能修改和删除
+     */
     @RequestMapping(value = "findForm")
-    public String findOne() {
-        return null;
+    public ShopDepositForm findForm() {
+        ShopDepositForm shopDepositForm = new ShopDepositForm();
+        return shopDepositForm;
+
     }
 
     @RequestMapping(value = "save")
-    public String save() {
-        return null;
+    public RestResponse save(@Valid ShopDepositForm shopDepositForm, BindingResult result) {
+
+        shopDepositService.save(shopDepositForm);
+        return new RestResponse("保存成功", ResponseCodeEnum.saved.name());
     }
 
-    private List<String> getActionList() {
-        return null;
-    }
+    @Autowired
+    private ShopDepositService shopDepositService;
 
 }
