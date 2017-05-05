461bf9ac8ce5a7a618c3093cedb9645628dc833e
lihx <lixh@qq.com>
2017-05-05 10:09:54 +0800

    update

--- summer-basic/src/main/java/net/myspring/basic/modules/hr/mapper/AccountMapper.java
+++ summer-basic/src/main/java/net/myspring/basic/modules/hr/mapper/AccountMapper.java
@@ -1,13 +1,19 @@
 package net.myspring.basic.modules.hr.mapper;
 
-import net.myspring.common.mybatis.MyMapper;
+import net.myspring.common.cache.IdCacheKeyGenerator;
 import net.myspring.basic.modules.hr.domain.Account;
 import net.myspring.basic.modules.hr.dto.AccountDto;
 import net.myspring.basic.modules.hr.web.query.AccountQuery;
+import net.myspring.common.mybatis.MyProvider;
+import net.myspring.mybatis.mapper.BaseMapper;
 import org.apache.ibatis.annotations.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import javax.cache.annotation.CacheDefaults;
+import javax.cache.annotation.CachePut;
+import javax.cache.annotation.CacheResult;
+import javax.cache.annotation.CacheValue;
 import java.util.List;
 import java.util.Map;
 
@@ -15,7 +21,30 @@ import java.util.Map;
  * Created by liuj on 2017/3/19.
  */
 @Mapper
-public interface AccountMapper extends MyMapper<Account,String> {
+@CacheDefaults(cacheName = "accounts")
+public interface AccountMapper extends BaseMapper<Account,String> {
+
+    @CacheResult
+    @SelectProvider(type = MyProvider.class, method = MyProvider.FIND_ONE)
+    Account findOne(String id);
+
+    @CachePut(cacheKeyGenerator = IdCacheKeyGenerator.class)
+    @InsertProvider(type = MyProvider.class, method = MyProvider.SAVE)
+    @Options(useGeneratedKeys = true)
+    int save(@CacheValue Account account);
+
+    @CachePut(cacheKeyGenerator = IdCacheKeyGenerator.class)
+    @UpdateProvider(type = MyProvider.class, method = MyProvider.UPDATE)
+    int update(@CacheValue Account account);
+
+    @UpdateProvider(type=MyProvider.class, method = MyProvider.LOGIC_DELETE_ONE)
+    int logicDeleteOne(String id);
+
+    @SelectProvider(type = MyProvider.class, method = MyProvider.FIND_ALL_ENABLED)
+    List<Account> findAllEnabled();
+
+    @SelectProvider(type = MyProvider.class, method = MyProvider.FIND_ALL)
+    List<Account> findAll();
 
     Account findByLoginName(String loginName);
 
--- summer-basic/src/main/java/net/myspring/basic/modules/hr/mapper/PositionMapper.java
+++ summer-basic/src/main/java/net/myspring/basic/modules/hr/mapper/PositionMapper.java
@@ -1,18 +1,46 @@
 package net.myspring.basic.modules.hr.mapper;
 
-import net.myspring.common.mybatis.MyMapper;
 import net.myspring.basic.modules.hr.domain.Position;
+import net.myspring.common.cache.IdCacheKeyGenerator;
 import net.myspring.basic.modules.hr.dto.PositionDto;
 import net.myspring.basic.modules.hr.web.query.PositionQuery;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import net.myspring.common.mybatis.MyProvider;
+import net.myspring.mybatis.mapper.BaseMapper;
+import org.apache.ibatis.annotations.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import javax.cache.annotation.CacheDefaults;
+import javax.cache.annotation.CachePut;
+import javax.cache.annotation.CacheResult;
+import javax.cache.annotation.CacheValue;
 import java.util.List;
 
 @Mapper
-public interface PositionMapper extends MyMapper<Position,String> {
+@CacheDefaults(cacheName = "positions")
+public interface PositionMapper extends BaseMapper<Position,String> {
+
+    @CacheResult
+    @SelectProvider(type = MyProvider.class, method = MyProvider.FIND_ONE)
+    Position findOne(String id);
+
+    @CachePut(cacheKeyGenerator = IdCacheKeyGenerator.class)
+    @InsertProvider(type = MyProvider.class, method = MyProvider.SAVE)
+    @Options(useGeneratedKeys = true)
+    int save(@CacheValue Position position);
+
+    @CachePut(cacheKeyGenerator = IdCacheKeyGenerator.class)
+    @UpdateProvider(type = MyProvider.class, method = MyProvider.UPDATE)
+    int update(@CacheValue Position position);
+
+    @UpdateProvider(type=MyProvider.class, method = MyProvider.LOGIC_DELETE_ONE)
+    int logicDeleteOne(String id);
+
+    @SelectProvider(type = MyProvider.class, method = MyProvider.FIND_ALL_ENABLED)
+    List<Position> findAllEnabled();
+
+    @SelectProvider(type = MyProvider.class, method = MyProvider.FIND_ALL)
+    List<Position> findAll();
 
     Page<PositionDto> findPage(Pageable pageable, @Param("p")PositionQuery positionQuery);
 
