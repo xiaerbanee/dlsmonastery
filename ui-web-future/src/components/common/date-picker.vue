b6851bdce7cbcd3583529239823fb6818105efdc
liuj <287724593@qq.com>
2017-05-08 00:10:36 +0800

    update

---
+++ ui-web-future/src/components/common/date-picker.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <el-date-picker  v-model="innerDate"  align="right"  @change="onChange" :picker-options="pickerDateOption"></el-date-picker>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['value'],
+    data() {
+      return {
+        pickerDateOption : util.pickerDateOption,
+        innerDate:null
+      }
+    },
+    methods:{
+      onChange( newVal) {
+        if(!newVal) {
+            newVal = "";
+        }
+        this.$emit('change', newVal);
+        return true;
+      }
+    },created () {
+        this.innerDate =new Date(this.value.replace(/-/,"/"));;
+    }
+  };
+</script>
