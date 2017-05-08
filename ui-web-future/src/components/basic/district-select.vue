7576929deec759dee6047585570e7744f3e1ad22
liuj <287724593@qq.com>
2017-05-08 16:58:44 +0800

    update

--- ui-web-future/src/components/basic/district-select.vue
+++ ui-web-future/src/components/basic/district-select.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <el-select v-model="innerDistrictId"  filterable remote :placeholder="$t('su_district.inputKey')" :remote-method="remoteSelect" :loading="remoteLoading"  :clearable=true @change="handleChange">
-      <el-option v-for="item in districts"  :key="item.id" :label="item.fullName" :value="item.id"></el-option>
+    <el-select v-model="innerId"  filterable remote :placeholder="$t('su_district.inputKey')" :remote-method="remoteSelect" :loading="remoteLoading"  :clearable=true @change="handleChange">
+      <el-option v-for="item in itemList"  :key="item.id" :label="item.fullName" :value="item.id"></el-option>
     </el-select>
   </div>
 </template>
@@ -10,35 +10,35 @@
     props: ['value'],
     data() {
       return {
-        innerDistrictId:this.value,
-        districts : [],
+        innerId:this.value,
+        itemList : [],
         remoteLoading:false
       };
     } ,methods:{
       remoteSelect(query) {
-        if(query == this.innerDistrictId) {
+        if(query == this.innerId || query == util.getLabel(this.itemList,this.innerId,"fullName")) {
             return;
         }
         if (query !== '') {
           this.remoteLoading = true;
           axios.get('/api/general/sys/district/search',{params:{key:query}}).then((response)=>{
-            this.districts=response.data;
+            this.itemList=response.data;
             this.remoteLoading = false;
           })
         } else {
-          this.districts = [];
+          this.itemList = [];
         }
       }, handleChange(newVal) {
         this.$emit('input', newVal);
         return true;
       },setValue(val) {
-        this.innerDistrictId=val;
+        this.innerId=val;
         if(val && val != "") {
-          var idList = util.getIdList(this.districts);
-          if(idList.indexOf(this.innerDistrictId) < 0) {
+          var idList = util.getIdList(this.itemList);
+          if(idList.indexOf(this.innerId) < 0) {
             this.remoteLoading = true;
-            axios.get('/api/general/sys/district/findById?id=' + this.innerDistrictId).then((response)=>{
-              this.districts=response.data;
+            axios.get('/api/general/sys/district/findById?id=' + this.innerId).then((response)=>{
+              this.itemList=response.data;
               this.remoteLoading = false;
             })
           }
