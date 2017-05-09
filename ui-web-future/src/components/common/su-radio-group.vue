6a685745b048d193f2148a2ea2cc8e2e4bd7c5ed
zhucc <1125517699@qq.com>
2017-05-09 12:03:28 +0800

    update

---
+++ ui-web-future/src/components/common/su-radio-group.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-radio-group v-model="innerBool" @change="onChange">
+      <el-radio :label=1>{{$t('depotForm.true')}}</el-radio>
+      <el-radio :label=0>{{$t('depotForm.false')}}</el-radio>
+    </el-radio-group>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['value'],
+    data() {
+      return {
+        innerBool:null
+      }
+    },
+    methods:{
+      onChange( newVal) {
+        if(!newVal) {
+          newVal = "";
+        }
+        this.$emit('input', newVal);
+        return true;
+      },setValue(val) {
+        var bool=null
+        if(util.isNotBlank(val)){
+            bool=parseInt(val)
+          this.innerBool=bool;
+
+        }
+      }
+    },created () {
+      this.setValue(this.value);
+    },watch: {
+      value :function (val) {
+        this.setValue(val);
+      }
+    }
+  };
+</script>
