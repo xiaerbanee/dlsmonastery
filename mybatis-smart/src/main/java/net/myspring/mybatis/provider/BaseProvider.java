ed7b60647b092b63553b2905ed8be2b02fccd627
liuj <287724593@qq.com>
2017-05-10 15:26:11 +0800

    update

--- mybatis-smart/src/main/java/net/myspring/mybatis/provider/BaseProvider.java
+++ mybatis-smart/src/main/java/net/myspring/mybatis/provider/BaseProvider.java
@@ -1,10 +1,12 @@
 package net.myspring.mybatis.provider;
 
 import com.google.common.collect.Maps;
+import com.sun.deploy.util.ReflectionUtil;
 import net.myspring.mybatis.context.MapperThreadLocal;
 import net.myspring.mybatis.context.ProviderContextUtils;
 import net.myspring.mybatis.dto.ColumnDto;
 import net.myspring.mybatis.dto.TableDto;
+import net.myspring.mybatis.exception.MybatisException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.ReflectionUtils;
@@ -47,7 +49,16 @@ public class BaseProvider<T, ID extends Serializable> {
                 insertable = columnDto.getInsertable();
             }
             if(getTableDto().getIdColumn() != null && getTableDto().getIdColumn().getJdbcColumn().equals(columnDto.getJdbcColumn())) {
+                Object id;
+                try {
+                    id = field.get(entity);
+                } catch (IllegalAccessException e) {
+                    throw new MybatisException(entity.getClass().getName() + " Can not get id value");
+                }
                 if (GenerationType.IDENTITY.name().equals(columnDto.getGeneratedValue().strategy().name())) {
+                    if(id != null) {
+                        throw new MybatisException(entity.getClass().getName() + "Can not insert object with id");
+                    }
                     insertable = false;
                 } else {
                     insertable = true;
