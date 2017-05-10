8f3517f2d5b786d38d1d2c63a16147c40666e483
zhucc <1125517699@qq.com>
2017-05-10 17:46:16 +0800

    update

--- ui-web-future/src/pages/future/basic/chainForm.vue
+++ ui-web-future/src/pages/future/basic/chainForm.vue
@@ -10,10 +10,7 @@
           <el-input v-model="inputForm.remarks"></el-input>
         </el-form-item>
         <el-form-item :label="$t('chainForm.shopType')" prop="depotList">
-          <el-select v-model="inputForm.depotList" multiple filterable remote :placeholder="$t('chainForm.inputKey')" :remote-method="remoteMethod"
-                     :loading="loading">
-            <el-option v-for="item in depotOptions" :key="item.id" :label="item.name" :value="item.id"></el-option>
-          </el-select>
+          <depot-select v-model="inputForm.depotList" category="SHOP"></depot-select>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" :disabled="submitDisabled" @click="formSubmit()">{{$t('chainForm.save')}}</el-button>
@@ -24,28 +21,28 @@
 </template>
 <script>
   import officeSelect from 'components/basic/office-select'
-
   import accountSelect from 'components/basic/account-select'
   import depotSelect from 'components/future/depot-select'
   export default{
       components:{
-          officeSelect
+          officeSelect,
           accountSelect,
-        depotSelect},
+          depotSelect
       },
     updated(){
-      if(this.pageUpdated) {
-        this.pageUpdated = false;
-        if(this.content != null && this.content.length>0) {
-          this.$refs['table'].clearSelection();
-          this.content.map((v,index)=>{
-            if(v.chainId ===this.inputForm.id){
-              this.$refs['table'].toggleRowSelection(this.content[index],true);
-            }
-          })
-          this.selectChange = true;
-        }
-      },
+    if (this.pageUpdated) {
+      this.pageUpdated = false;
+      if (this.content != null && this.content.length > 0) {
+        this.$refs['table'].clearSelection();
+        this.content.map((v, index) => {
+          if (v.chainId === this.inputForm.id) {
+            this.$refs['table'].toggleRowSelection(this.content[index], true);
+          }
+        })
+        this.selectChange = true;
+      }
+    }
+  },
     data(){
       return{
         isCreate:this.$route.query.id==null,
@@ -58,7 +55,6 @@
             remarks:""
         },
 //        states:[],
-        depotOptions:[],
         rules: {
           name: [{ required: true, message: this.$t('chainForm.prerequisiteMessage')}]
         }
@@ -84,22 +80,6 @@
             this.submitDisabled = false;
           }
         })
-      },remoteMethod(query){
-        if (query !== '') {
-          console.log("query "+query);
-          this.loading = true;
-            axios.get('/api/ws/future/basic/depot/searchShop',{params:{name:query}}).then((response)=>{
-
-              this.depotOptions = response.data;
-
-              console.log(this.depotOptions);
-
-              this.loading = false;
-            });
-        } else {
-          this.depotOptions = [];
-        }
-
       }
       },created(){
         axios.get('/api/ws/future/basic/chain/findForm',{params: {id:this.$route.query.id}}).then((response)=>{
