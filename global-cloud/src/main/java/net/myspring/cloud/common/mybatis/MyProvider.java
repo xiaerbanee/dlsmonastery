aa32fa8e28cef2fd5a90684e4772602f512d108e
liuj <287724593@qq.com>
2017-05-10 11:52:07 +0800

    update

--- global-cloud/src/main/java/net/myspring/cloud/common/mybatis/MyProvider.java
+++ global-cloud/src/main/java/net/myspring/cloud/common/mybatis/MyProvider.java
@@ -1,6 +1,8 @@
 package net.myspring.cloud.common.mybatis;
 
 import com.google.common.collect.Lists;
+import net.myspring.cloud.common.domain.CompanyEntity;
+import net.myspring.cloud.common.utils.RequestUtils;
 import net.myspring.mybatis.provider.CrudProvider;
 import net.myspring.util.text.StringUtils;
 import org.slf4j.Logger;
@@ -19,7 +21,7 @@ import java.util.Map;
     public static final String FIND_ALL_ENABLED="findAllEnabled";
 
     public String logicDeleteOne(Object id) {
-        String sql =  "UPDATE " + getTableDto().getJdbcTable() + " SET enabled=0 WHERE " + getTableDto().getIdColumn().getJdbcColumn() + "=#{id}";
+        String sql =  "UPDATE " + getTableDto().getJdbcTable() + " SET enabled=0 WHERE " + getTableDto().getIdColumn().getJdbcColumn() + "=#{id}" + getCompanyFilter();
         logger.info(sql);
         return sql;
     }
@@ -30,15 +32,24 @@ import java.util.Map;
         for(int i =0;i<list.size();i++) {
             values.add("#{list[" + i + "]}");
         }
-        String sql = "UPDATE " + getTableDto().getJdbcTable() + " SET enabled=0 WHERE " + getTableDto().getIdColumn().getJdbcColumn() + " IN (" + StringUtils.join(values,",") + ")";
+        String sql = "UPDATE " + getTableDto().getJdbcTable() + " SET enabled=0 WHERE " + getTableDto().getIdColumn().getJdbcColumn() + " IN (" + StringUtils.join(values,",") + ")"+ getCompanyFilter();
         logger.info(sql);
         return sql;
     }
 
     public String findAllEnabled() {
-        String sql = "SELECT  * FROM " + getTableDto().getJdbcTable()+" where enabled=1";
+        String sql = "SELECT  * FROM " + getTableDto().getJdbcTable()+" where enabled=1"+ getCompanyFilter();
         logger.info(sql);
         return sql;
+    }
+
+
+    private String getCompanyFilter() {
+        String companyFilter = "";
+        if(getDomainClass().isAssignableFrom(CompanyEntity.class)) {
+            companyFilter =  " and company_id = "+ RequestUtils.getCompanyId();
+        }
+        return companyFilter;
     }
 
 }
