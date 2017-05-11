116749cd50c4e7220a51da7e7c9299c98343f98e
lihx <lixh@qq.com>
2017-05-11 10:06:49 +0800

    delete report

---
+++ global-cloud/src/main/java/net/myspring/cloud/modules/input/dto/ModelDto.java
@@ -0,0 +1,7 @@
+package net.myspring.cloud.modules.input.dto;
+
+/**
+ * Created by lihx on 2017/5/11.
+ */
+public class ModelDto {
+}
--- global-cloud/src/main/java/net/myspring/cloud/modules/input/web/query/BatchOtherRecAbleQuery.java
+++
@@ -1,9 +0,0 @@
-package net.myspring.cloud.modules.input.web.query;
-
-import net.myspring.cloud.common.query.BaseQuery;
-
-/**
- * Created by lihx on 2017/5/9.
- */
-public class BatchOtherRecAbleQuery extends BaseQuery {
-}
