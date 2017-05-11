21b8c92080d96746948b3566cf8c8805745856cb
lihx <lixh@qq.com>
2017-05-11 18:22:52 +0800

    update

---
+++ global-cloud/src/main/java/net/myspring/cloud/modules/input/dto/NameNumberDto.java
@@ -0,0 +1,25 @@
+package net.myspring.cloud.modules.input.dto;
+
+/**
+ * Created by lihx on 2017/5/11.
+ */
+public class NameNumberDto {
+    private String name;
+    private String number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
