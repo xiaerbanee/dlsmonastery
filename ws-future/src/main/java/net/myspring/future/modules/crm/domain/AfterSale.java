87bc523c8832767a8ba17a31d786b0fff7815b06
liuj <287724593@qq.com>
2017-05-12 09:42:59 +0800

    售后相关

--- ws-future/src/main/java/net/myspring/future/modules/crm/domain/AfterSale.java
+++ ws-future/src/main/java/net/myspring/future/modules/crm/domain/AfterSale.java
@@ -1,49 +1,84 @@
 package net.myspring.future.modules.crm.domain;
 
 
-import com.google.common.collect.Lists;
 import net.myspring.future.common.domain.CompanyEntity;
-import net.myspring.future.modules.basic.domain.Depot;
-import net.myspring.future.modules.basic.domain.Product;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Table(name="crm_after_sale")
 public class AfterSale extends CompanyEntity<AfterSale> {
     private String businessId;
+    private String badProductId;
     private String badProductImeId;
+    private String badDepotId;
     private String badType;
-    private String replaceProductImeId;
-    private String toAreaProductImeId;
     private String packageStatus;
     private String memory;
-    private String toStoreType;
-    private String toStoreRemarks;
-    private LocalDate toStoreDate;
-    private LocalDate toCompanyDate;
-    private String toCompanyRemarks;
-    private LocalDate fromCompanyDate;
-    private String fromCompanyProductId;
-    private String areaDepotId;
-    private Boolean fromAreaToFinance;
-    private Boolean toAreaToFinance;
-    private Boolean fromCompanyToFinance;
-    private Boolean toCompanyToFinance;
-    private String badDepotId;
-    private LocalDateTime toFinanceDate;
     private Integer version = 0;
-    private Depot depot;
-    private String depotId;
 
-    private ProductIme badProductIme;
-    private ProductIme replaceProductIme;
-    private ProductIme toAreaProductIme;
-    private Depot badDepot;
-    private Depot areaDepot;
-    private Product fromCompanyProduct;
+    public String getBusinessId() {
+        return businessId;
+    }
+
+    public void setBusinessId(String businessId) {
+        this.businessId = businessId;
+    }
+
+    public String getBadProductId() {
+        return badProductId;
+    }
+
+    public void setBadProductId(String badProductId) {
+        this.badProductId = badProductId;
+    }
+
+    public String getBadProductImeId() {
+        return badProductImeId;
+    }
+
+    public void setBadProductImeId(String badProductImeId) {
+        this.badProductImeId = badProductImeId;
+    }
+
+    public String getBadDepotId() {
+        return badDepotId;
+    }
+
+    public void setBadDepotId(String badDepotId) {
+        this.badDepotId = badDepotId;
+    }
+
+    public String getBadType() {
+        return badType;
+    }
+
+    public void setBadType(String badType) {
+        this.badType = badType;
+    }
+
+    public String getPackageStatus() {
+        return packageStatus;
+    }
+
+    public void setPackageStatus(String packageStatus) {
+        this.packageStatus = packageStatus;
+    }
+
+    public String getMemory() {
+        return memory;
+    }
+
+    public void setMemory(String memory) {
+        this.memory = memory;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
 }
--- ws-future/src/main/java/net/myspring/future/modules/crm/domain/AfterSaleDetail.java
+++ ws-future/src/main/java/net/myspring/future/modules/crm/domain/AfterSaleDetail.java
@@ -1,11 +1,85 @@
 package net.myspring.future.modules.crm.domain;
 
+import net.myspring.future.common.domain.CompanyEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by liuj on 2017/5/11.
+ *
  */
-public class AfterSaleDetail {
+@Entity
+@Table(name="crm_after_sale_detail")
+public class AfterSaleDetail extends CompanyEntity<AfterSaleDetail>{
+    //售后节点：地区录入，总部录入，坏机返厂，好机返库
+    private String status;
+    //坏机来源
+    private String fromDepotId;
+    //坏机所在库
+    private String toDepotId;
+    //替换机串码
+    private String replaceProductImeId;
+    //替换机型号
+    private String replaceProductId;
+    //坏机录入金蝶单据
+    private  String inputOutCode;
+    //替换机录入对应金蝶单据
+    private  String replaceOutCode;
 
-    private String type;
+    public String getStatus() {
+        return status;
+    }
 
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
+    public String getFromDepotId() {
+        return fromDepotId;
+    }
+
+    public void setFromDepotId(String fromDepotId) {
+        this.fromDepotId = fromDepotId;
+    }
+
+    public String getToDepotId() {
+        return toDepotId;
+    }
+
+    public void setToDepotId(String toDepotId) {
+        this.toDepotId = toDepotId;
+    }
+
+    public String getReplaceProductImeId() {
+        return replaceProductImeId;
+    }
+
+    public void setReplaceProductImeId(String replaceProductImeId) {
+        this.replaceProductImeId = replaceProductImeId;
+    }
+
+    public String getReplaceProductId() {
+        return replaceProductId;
+    }
+
+    public void setReplaceProductId(String replaceProductId) {
+        this.replaceProductId = replaceProductId;
+    }
+
+    public String getInputOutCode() {
+        return inputOutCode;
+    }
+
+    public void setInputOutCode(String inputOutCode) {
+        this.inputOutCode = inputOutCode;
+    }
+
+    public String getReplaceOutCode() {
+        return replaceOutCode;
+    }
+
+    public void setReplaceOutCode(String replaceOutCode) {
+        this.replaceOutCode = replaceOutCode;
+    }
 }
