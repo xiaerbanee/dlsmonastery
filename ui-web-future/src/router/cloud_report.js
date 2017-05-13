1ce3bda8d1dddbc48c1e84bef46c2b420b27fa09
lihx <lixh@qq.com>
2017-05-13 14:17:49 +0800

    update

--- ui-web-future/src/router/cloud_report.js
+++ ui-web-future/src/router/cloud_report.js
@@ -1,11 +1,11 @@
 const payableReport  = r => require.ensure([], () => r(require('pages/cloud/report/payableReport.vue')));
-const receivableReport  = r => require.ensure([], () => r(require('pages/cloud/report/receivableReport.vue')));
+const customerReceive  = r => require.ensure([], () => r(require('pages/cloud/report/customerReceive.vue')));
 const consignmentReport  = r => require.ensure([], () => r(require('pages/cloud/report/consignmentReport.vue')));
 const retailReport  = r => require.ensure([], () => r(require('pages/cloud/report/retailReport.vue')));
 
 let routes = [
   {path: '/cloud/report/payableReport',component: payableReport,name: 'payableReport'},
-  {path: '/cloud/report/receivableReport',component: receivableReport,name: 'receivableReport'},
+  {path: '/cloud/report/customerReceive',component: customerReceive,name: 'customerReceive'},
   {path: '/cloud/report/consignmentReport',component: consignmentReport,name: 'consignmentReport'},
   {path: '/cloud/report/retailReport',component: retailReport,name: 'retailReport'},
 ];
