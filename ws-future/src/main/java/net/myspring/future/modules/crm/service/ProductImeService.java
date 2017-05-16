bf14a33f93e1b4edef168994e9bbbcfdfbb9073c
liuj <287724593@qq.com>
2017-05-16 20:43:55 +0800

    串码列表

--- ws-future/src/main/java/net/myspring/future/modules/crm/service/ProductImeService.java
+++ ws-future/src/main/java/net/myspring/future/modules/crm/service/ProductImeService.java
@@ -2,6 +2,7 @@ package net.myspring.future.modules.crm.service;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import net.myspring.basic.modules.sys.dto.DictEnumDto;
 import net.myspring.future.common.utils.CacheUtils;
 import net.myspring.future.modules.basic.domain.Product;
 import net.myspring.future.modules.basic.mapper.ProductMapper;
@@ -10,6 +11,7 @@ import net.myspring.future.modules.crm.dto.ProductImeDto;
 import net.myspring.future.modules.crm.mapper.ProductImeMapper;
 import net.myspring.future.modules.crm.mapper.ProductImeSaleMapper;
 import net.myspring.future.modules.crm.mapper.ProductImeUploadMapper;
+import net.myspring.future.modules.crm.web.query.ProductImeQuery;
 import net.myspring.util.collection.CollectionUtil;
 import net.myspring.util.excel.SimpleExcelColumn;
 import net.myspring.util.excel.SimpleExcelSheet;
@@ -17,6 +19,7 @@ import net.myspring.util.mapper.BeanUtil;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -29,41 +32,15 @@ public class ProductImeService {
     @Autowired
     private ProductImeMapper productImeMapper;
     @Autowired
-    private ProductImeSaleMapper productImeSaleMapper;
-    @Autowired
-    private ProductImeUploadMapper productImeUploadMapper;
-    @Autowired
-    private ProductMapper productMapper;
-    @Autowired
     private CacheUtils cacheUtils;
 
-
-    public List<ProductImeDto> findByImeList(List<String> imeList){
-        List<ProductIme> productImeList=productImeMapper.findByImeList(imeList);
-        List<ProductImeDto> productImeDtoList= BeanUtil.map(productImeList,ProductImeDto.class);
-        cacheUtils.initCacheInput(productImeDtoList);
-        return productImeDtoList;
+    //分页，但不查询总数
+    public Page<ProductImeDto> findPage(Pageable pageable,ProductImeQuery productImeQuery) {
+        productImeQuery.setPageable(pageable);
+        List<ProductImeDto> productImeList = productImeMapper.findList(productImeQuery);
+        cacheUtils.initCacheInput(productImeList);
+        Page<ProductImeDto> page = new PageImpl(productImeList,pageable,(pageable.getPageNumber()+100)*pageable.getPageSize());
+        return page;
     }
-
-    public Map<String,Integer> findQtyMap(List<String> imeList){
-        Map<String,Integer> map= Maps.newHashMap();
-        List<ProductIme> productImeList=productImeMapper.findByImeList(imeList);
-        if(CollectionUtil.isNotEmpty(productImeList)){
-            List<Product> productList=productMapper.findByIds(CollectionUtil.extractToList(productImeList,"productId"));
-            Map<String,Product> productMap=CollectionUtil.extractToMap(productList,"id");
-            Map<String,List<ProductIme>> productImeMap=CollectionUtil.extractToMapList(productImeList,"productId");
-            for(Map.Entry<String,List<ProductIme>> entry:productImeMap.entrySet()){
-                map.put(productMap.get(entry.getKey()).getName(),entry.getValue().size());
-            }
-        }
-        return map;
-    }
-
-    public ProductIme findOne(String id){
-        ProductIme productIme = productImeMapper.findOne(id);
-        return productIme;
-    }
-
-
 
 }
--- ws-future/src/main/java/net/myspring/future/modules/crm/web/controller/ProductImeController.java
+++ ws-future/src/main/java/net/myspring/future/modules/crm/web/controller/ProductImeController.java
@@ -1,13 +1,18 @@
 package net.myspring.future.modules.crm.web.controller;
 
 
+import net.myspring.basic.modules.sys.dto.DictEnumDto;
 import net.myspring.future.modules.basic.domain.Depot;
+import net.myspring.future.modules.crm.domain.ProductIme;
+import net.myspring.future.modules.crm.dto.ProductImeDto;
 import net.myspring.future.modules.crm.service.ProductImeService;
+import net.myspring.future.modules.crm.web.query.ProductImeQuery;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,43 +29,8 @@ public class ProductImeController {
     private ProductImeService productImeService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String list(HttpServletRequest request){
-        return null;
+    public Page list(Pageable pageable,ProductImeQuery productImeQuery){
+        Page<ProductImeDto> page = productImeService.findPage(pageable,productImeQuery);
+        return page;
     }
-
-    @RequestMapping(value = "getQuery")
-    public String getQuery(Depot depot) {
-        return null;
-    }
-
-
-    @RequestMapping(value = "search")
-    public String findByImeList(String imeStr ) {
-        return null;
-    }
-
-    @RequestMapping(value = "searchIme")
-    public String searchIme(String ime ) {
-        return null;
-    }
-
-    @RequestMapping(value = "searchByStore")
-    public String searchByStore(String depotId,String ime ) {
-        return null;
-    }
-
-    @RequestMapping(value = "detail")
-    public String detail(String productImeId){
-        return null;
-    }
-
-    @RequestMapping(value = "export", method = RequestMethod.GET)
-    public ModelAndView export(HttpServletRequest request) {
-        return null;
-    }
-
-    private List<String> getActionList() {
-        return null;
-    }
-
 }
