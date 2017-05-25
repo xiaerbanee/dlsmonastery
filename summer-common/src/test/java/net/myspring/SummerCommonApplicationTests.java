dd775e5a1d8a464cfa99b6a7375c888af2cb328b
liuj <287724593@qq.com>
2017-05-25 19:26:24 +0800

    update

--- summer-common/src/test/java/net/myspring/SummerCommonApplicationTests.java
+++ summer-common/src/test/java/net/myspring/SummerCommonApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SummerBeanApplicationTests {
+public class SummerCommonApplicationTests {
 
 	@Test
 	public void contextLoads() {
