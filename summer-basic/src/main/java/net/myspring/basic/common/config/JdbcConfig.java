e063ebb07dca10727a5a5c50b32add62577fc7d6
liuj <287724593@qq.com>
2017-05-26 11:10:49 +0800

    update

---
+++ summer-basic/src/main/java/net/myspring/basic/common/config/JdbcConfig.java
@@ -0,0 +1,25 @@
+package net.myspring.basic.common.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JdbcConfig {
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public JdbcTemplate JdbcTemplate() {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+}
