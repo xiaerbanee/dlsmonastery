66edd2f9bf92c9195c88bd46034297de523b6253
liuj <287724593@qq.com>
2017-05-26 18:20:46 +0800

    update

--- summer-basic/src/main/java/net/myspring/basic/modules/sys/repository/OfficeRepository.kt
+++ summer-basic/src/main/java/net/myspring/basic/modules/sys/repository/OfficeRepository.kt
@@ -1,7 +1,10 @@
 package net.myspring.basic.modules.sys.repository
 
+import com.google.common.collect.Maps
+import net.myspring.basic.common.config.MyBeanPropertyRowMapper
 import net.myspring.basic.common.repository.BaseRepository
 import net.myspring.basic.modules.sys.domain.Office
+import net.myspring.basic.modules.sys.dto.BackendMenuDto
 import net.myspring.basic.modules.sys.dto.OfficeDto
 import net.myspring.basic.modules.sys.web.query.OfficeQuery
 import net.myspring.util.repository.QueryUtils
@@ -13,6 +16,7 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import javax.persistence.EntityManager
 
 
@@ -36,77 +40,77 @@ interface OfficeRepository :BaseRepository<Office,String>,OfficeRepositoryCustom
     fun findByOfficeRuleName(officeRuleName:String):MutableList<Office>
 
     @Query("""
-    SELECT t1.*
-        FROM sys_office t1,hr_account t3
+        SELECT t1
+        FROM  #{#entityName} t1,Account t2
         where t1.enabled=1
-        and t1.id=t3.office_id
-        and t3.id=?1
-     """, nativeQuery = true)
+        and t1.id=t2.officeId
+        and t2.id=?1
+     """)
     fun findByAccountId(accountId:String):Office
 
     @Query("""
-     SELECT t1.*
-        FROM sys_office t1,hr_account t3
+        SELECT t1
+        FROM  #{#entityName} t1,Account t2
         where t1.enabled=1
-        and t1.id=t3.office_id
-        and t3.account_id in ?1
-     """, nativeQuery = true)
+        and t1.id=t2.officeId
+        and t2.accountId in ?1
+     """)
     fun findByAccountIds(accountIds:MutableList<String>):MutableList<Office>
 
     @Query("""
-        SELECT t1.*
-        FROM sys_office t1
-        where t1.parent_id is NULL
-     """, nativeQuery = true)
+        SELECT t
+        FROM  #{#entityName} t
+        where t.parentId is NULL
+     """)
     fun findParentIdIsNull(): Office
 
     @Query("""
-       SELECT t1.*
-        FROM sys_office t1
-        where t1.parent_ids like %?1%
-        and t1.enabled =1
-     """, nativeQuery = true)
+        SELECT t
+        FROM  #{#entityName} t
+        where t.parent_ids like %?1%
+        and t.enabled =1
+     """)
     fun findByParentIdsLike(parentId: String): MutableList<Office>
 
     @Query("""
-       SELECT t1.id,t1.name
-        FROM sys_office t1
-        WHERE t1.enabled = 1
-        and t1.id in ?1
-     """, nativeQuery = true)
+        SELECT t.id,t.name
+        FROM  #{#entityName} t
+        WHERE t.enabled = 1
+        and t.id in ?1
+     """)
     fun findByIds(ids: MutableList<String>): MutableList<Office>
 
     @Query("""
-        SELECT t1.*
-        FROM sys_office t1,sys_office_rule t2
+        SELECT t1
+        FROM  #{#entityName} t1,OfficeRule t2
         where t1.enabled=1
         and t2.enabled=1
-        and t1.office_rule_id=t2.id
+        and t1.officeRuleId=t2.id
         and (t1.parent_ids like %?1% or t1.id=?2)
-        and t1.office_rule_id=?2
-     """, nativeQuery = true)
+        and t1.officeRuleId=?2
+     """)
     fun findByOfficeIdAndRuleId( officeId: String, officeRuleId: String): Office
 
     @Query("""
-       SELECT t1.*
-        FROM sys_office t1
-        WHERE t1.enabled=1
-        and t1.area_id  = (
+        SELECT t
+        FROM  #{#entityName} t
+        WHERE t.enabled=1
+        and t.areaId  = (
             SELECT
-            t2.area_id
+            t2.areaId
             FROM
-            sys_office t2
+            Office t2
             WHERE
             t2.id = ?1
          )
-     """, nativeQuery = true)
+     """)
     fun findSameAreaByOfficeId(officeId: String): MutableList<Office>
 
 
 
     @Query("""
-       SELECT t1.*
-        FROM sys_office t1
+        SELECT t
+        FROM  #{#entityName} t
         WHERE t1.enabled=1
      """, nativeQuery = true)
     fun findAllEnabled():MutableList<Office>
@@ -117,14 +121,14 @@ interface OfficeRepositoryCustom {
 
     fun findByFilter(officeQuery: OfficeQuery): MutableList<Office>
 
-    fun findByFilterAll(@Param("p") map: Map<String, Any>): MutableList<Office>
+    fun findByFilterAll(map: Map<String, Any>): MutableList<Office>
 
     fun findByAreaIds(areaIds: MutableList<String>): MutableList<Office>
 
     fun findPage(pageable: Pageable, officeQuery: OfficeQuery): Page<OfficeDto>?
 }
 
-class OfficeRepositoryImpl@Autowired constructor(val entityManager: EntityManager): OfficeRepositoryCustom {
+class OfficeRepositoryImpl@Autowired constructor(val namedParameterJdbcTemplate: NamedParameterJdbcTemplate): OfficeRepositoryCustom {
     override fun findPage(pageable: Pageable, officeQuery: OfficeQuery): Page<OfficeDto>? {
         return null;
     }
@@ -137,17 +141,17 @@ class OfficeRepositoryImpl@Autowired constructor(val entityManager: EntityManage
             where  t1.enabled =1
             and (
         """)
-        for((index,value) in parentIdList.withIndex()) {
+        for((index) in parentIdList.withIndex()) {
             sb.append(" t1.parent_ids like :parentId").append(index);
             if(index < parentIdList.size-1) {
                 sb.append(" or ");
             }
         }
-        var query = entityManager.createNativeQuery(sb.toString(),Office::class.java);
+        var paramMap = Maps.newHashMap<String,String>();
         for((index,value) in parentIdList.withIndex()) {
-            query.setParameter("parentId" + index ,"%$value%");
+            paramMap.put("parentId" + index ,"%$value%");
         }
-        return query.resultList as MutableList<Office>;
+        return namedParameterJdbcTemplate.query(sb.toString(),paramMap, MyBeanPropertyRowMapper(Office::class.java))
     }
 
     override fun findByFilter(officeQuery: OfficeQuery): MutableList<Office> {
@@ -169,9 +173,7 @@ class OfficeRepositoryImpl@Autowired constructor(val entityManager: EntityManage
             order by office.name
             limit 0,20
         """)
-        var query = entityManager.createNativeQuery(sb.toString());
-        QueryUtils.setParameter(query,officeQuery);
-        return query.resultList as MutableList<Office>;
+        return namedParameterJdbcTemplate.query(sb.toString(),QueryUtils.getParamMap(officeQuery), MyBeanPropertyRowMapper(Office::class.java))
     }
 
     override fun findByFilterAll(map: Map<String, Any>): MutableList<Office> {
@@ -183,8 +185,7 @@ class OfficeRepositoryImpl@Autowired constructor(val entityManager: EntityManage
             where
             office.enabled=1
         """)
-        var query = entityManager.createNativeQuery(sb.toString(),Office::class.java);
-        return query.resultList as MutableList<Office>;
+        return namedParameterJdbcTemplate.query(sb.toString(),MyBeanPropertyRowMapper(Office::class.java))
     }
 
     override fun findByAreaIds(areaIds: MutableList<String>): MutableList<Office> {
@@ -196,18 +197,18 @@ class OfficeRepositoryImpl@Autowired constructor(val entityManager: EntityManage
             and (
             t1.id IN :areaIds or
         """)
-        for((index,value) in areaIds.withIndex()) {
+        for((index) in areaIds.withIndex()) {
             sb.append(" t1.parent_ids like :parentId").append(index);
             if(index < areaIds.size-1) {
                 sb.append(" or ");
             }
         }
-        var query = entityManager.createNativeQuery(sb.toString(),Office::class.java);
-        query.setParameter("areaIds",areaIds);
+        var paramMap = Maps.newHashMap<String,Any>();
+        paramMap.put("areaIds",areaIds);
         for((index,value) in areaIds.withIndex()) {
-            query.setParameter("parentId" + index ,"%$value%");
+            paramMap.put("parentId" + index ,"%$value%");
         }
-        return query.resultList as MutableList<Office>;
+        return namedParameterJdbcTemplate.query(sb.toString(),paramMap, MyBeanPropertyRowMapper(Office::class.java));
     }
 
 }
