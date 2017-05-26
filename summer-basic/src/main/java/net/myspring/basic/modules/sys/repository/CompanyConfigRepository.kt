41775d73a50e36759aa3b7a3aa9647353d86ccd2
liuj <287724593@qq.com>
2017-05-26 15:37:27 +0800

    update

--- summer-basic/src/main/java/net/myspring/basic/modules/sys/repository/CompanyConfigRepository.kt
+++ summer-basic/src/main/java/net/myspring/basic/modules/sys/repository/CompanyConfigRepository.kt
@@ -1,7 +1,9 @@
 package net.myspring.basic.modules.sys.repository
 
+import net.myspring.basic.common.config.MyBeanPropertyRowMapper
 import net.myspring.basic.common.repository.BaseRepository
 import net.myspring.basic.modules.sys.domain.CompanyConfig
+import net.myspring.basic.modules.sys.dto.BackendMenuDto
 import net.myspring.basic.modules.sys.dto.CompanyConfigCacheDto
 import net.myspring.basic.modules.sys.dto.CompanyConfigDto
 import net.myspring.basic.modules.sys.dto.DictEnumDto
@@ -14,6 +16,8 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import java.util.*
 import javax.persistence.EntityManager
 
 /**
@@ -27,30 +31,31 @@ interface CompanyConfigRepository :BaseRepository<CompanyConfig,String>, Company
     override fun findOne(id: String): CompanyConfig
 
     @Query("""
-        SELECT t1.*
-        FROM sys_company_config t1
-        where t1.enabled=1
-        and t1.code=:code
-     """, nativeQuery = true)
+        SELECT t
+        FROM  #{#entityName} t
+        where t.enabled=1
+        and t.code=:code
+     """)
     fun  findByCode(@Param("code")code:String):CompanyConfig
-
-    @Query("""
-            SELECT t1.*
-        FROM sys_company_config t1
-        where t1.enabled=1
-     """, nativeQuery = true)
-    fun  findAllCache():MutableList<CompanyConfigCacheDto>
-
-
 }
 
 interface CompanyConfigRepositoryCustom{
+    fun  findAllCache():MutableList<CompanyConfigCacheDto>
+
     fun findPage(pageable: Pageable, companyConfigQuery: CompanyConfigQuery): Page<CompanyConfigDto>?
 
 
 }
 
-class CompanyConfigRepositoryImpl @Autowired constructor(val entityManager: EntityManager): CompanyConfigRepositoryCustom{
+class CompanyConfigRepositoryImpl @Autowired constructor(val namedParameterJdbcTemplate: NamedParameterJdbcTemplate): CompanyConfigRepositoryCustom{
+    override fun findAllCache(): MutableList<CompanyConfigCacheDto> {
+        return namedParameterJdbcTemplate.query("""
+                 SELECT t1.*
+                FROM sys_company_config t1
+                where t1.enabled=1
+                """, MyBeanPropertyRowMapper(CompanyConfigCacheDto::class.java));
+    }
+
     override fun findPage(pageable: Pageable, companyConfigQuery: CompanyConfigQuery): Page<CompanyConfigDto>? {
         return null
     }
