8727df11496a2f61cc2f3414565c93f1c7a99f48
liuj <287724593@qq.com>
2017-05-27 15:15:17 +0800

    update

---
+++ summer-basic/src/main/java/net/myspring/basic/common/config/AuditorConfig.java
@@ -0,0 +1,19 @@
+package net.myspring.basic.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * Created by liuj on 2017/5/19.
+ */
+@Configuration
+@EnableJpaAuditing
+public class AuditorConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new SecurityAuditorConfig();
+    }
+}
