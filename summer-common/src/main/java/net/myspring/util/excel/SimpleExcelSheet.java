b343ff35a3083d1c44dba5f19b8fd30252e9b086
liuj <287724593@qq.com>
2017-05-28 14:28:45 +0800

    export

--- summer-common/src/main/java/net/myspring/util/excel/SimpleExcelSheet.java
+++ summer-common/src/main/java/net/myspring/util/excel/SimpleExcelSheet.java
@@ -3,7 +3,6 @@ package net.myspring.util.excel;
 import com.google.common.collect.Lists;
 
 import java.util.List;
-import java.util.function.Function;
 
 /**
  * Created by liuj on 2017/2/16.
@@ -11,29 +10,12 @@ import java.util.function.Function;
 public class SimpleExcelSheet {
     private String sheetName;
     private List<Object> dataList;
-    private Function<Object, List> rowExpander;
-
     private List<SimpleExcelColumn> simpleExcelColumnList = Lists.newArrayList();
 
     public SimpleExcelSheet(String sheetName,List dataList,List<SimpleExcelColumn> simpleExcelColumnList) {
         this.sheetName = sheetName;
         this.dataList = dataList;
         this.simpleExcelColumnList = simpleExcelColumnList;
-    }
-
-    public SimpleExcelSheet(String sheetName,List dataList,List<SimpleExcelColumn> simpleExcelColumnList, Function<Object, List> rowExpander) {
-        this.sheetName = sheetName;
-        this.dataList = dataList;
-        this.simpleExcelColumnList = simpleExcelColumnList;
-        this.rowExpander = rowExpander;
-    }
-
-    public Function<Object, List> getRowExpander() {
-        return rowExpander;
-    }
-
-    public void setRowExpander(Function<Object, List> rowExpander) {
-        this.rowExpander = rowExpander;
     }
 
     public String getSheetName() {
