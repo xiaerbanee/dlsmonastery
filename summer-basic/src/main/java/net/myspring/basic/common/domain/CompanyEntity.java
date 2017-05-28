de554bcb9466ffb19076a2b7dece40ecf15b0bc7
liuj <287724593@qq.com>
2017-05-28 17:42:44 +0800

    export

--- summer-basic/src/main/java/net/myspring/basic/common/domain/CompanyEntity.java
+++ summer-basic/src/main/java/net/myspring/basic/common/domain/CompanyEntity.java
@@ -2,6 +2,10 @@ package net.myspring.basic.common.domain;
 
 import net.myspring.basic.common.utils.RequestUtils;
 import org.apache.commons.lang.StringUtils;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.Filters;
+import org.hibernate.annotations.ParamDef;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
@@ -11,6 +15,8 @@ import javax.persistence.PrePersist;
  * Created by liuj on 2017/4/12.
  */
 @MappedSuperclass
+@FilterDef(name = "companyFilter", parameters = { @ParamDef(name = "companyId", type = "string") })
+@Filters({ @Filter(name = "companyFilter", condition = "company_id =:companyId") })
 public class CompanyEntity<T> extends DataEntity<T> {
 
     @Column(updatable = false)
