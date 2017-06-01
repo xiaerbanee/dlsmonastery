0590d960370c11eeca1a301c140558ee05bea354
haos <1647424873@qq.com>
2017-06-01 17:20:07 +0800

    update

--- ws-future/src/main/java/net/myspring/future/modules/crm/web/controller/ReportScoreOfficeController.java
+++ ws-future/src/main/java/net/myspring/future/modules/crm/web/controller/ReportScoreOfficeController.java
@@ -1,6 +1,13 @@
 package net.myspring.future.modules.crm.web.controller;
 
 
+import net.myspring.future.modules.crm.domain.ReportScoreOffice;
+import net.myspring.future.modules.crm.dto.ReportScoreOfficeDto;
+import net.myspring.future.modules.crm.service.ReportScoreOfficeService;
+import net.myspring.future.modules.crm.web.query.ReportScoreOfficeQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,11 +17,13 @@ import java.util.Map;
 @RequestMapping(value = "crm/reportScoreOffice")
 public class ReportScoreOfficeController {
 
+    @Autowired
+    ReportScoreOfficeService reportScoreOfficeService;
 
-
-    @RequestMapping
-    public String list(HttpServletRequest request){
-        return null;
+    @RequestMapping(method = RequestMethod.GET)
+    public Page<ReportScoreOfficeDto> list(Pageable pageable, ReportScoreOfficeQuery reportScoreOfficeQuery){
+        Page<ReportScoreOfficeDto> page=reportScoreOfficeService.findPage(pageable,reportScoreOfficeQuery);
+        return page;
     }
 
     @RequestMapping(value="getQuery",method = RequestMethod.GET)
---
+++ ws-future/src/main/java/net/myspring/future/modules/crm/web/query/ReportScoreOfficeQuery.java
@@ -0,0 +1,39 @@
+package net.myspring.future.modules.crm.web.query;
+
+import net.myspring.future.common.query.BaseQuery;
+
+import java.time.LocalDate;
+
+/**
+ * Created by haos on 2017/6/1.
+ */
+public class ReportScoreOfficeQuery extends BaseQuery {
+
+    private LocalDate scoreDate;
+    private String officeId;
+    private String areaId;
+
+    public LocalDate getScoreDate() {
+        return scoreDate;
+    }
+
+    public void setScoreDate(LocalDate scoreDate) {
+        this.scoreDate = scoreDate;
+    }
+
+    public String getOfficeId() {
+        return officeId;
+    }
+
+    public void setOfficeId(String officeId) {
+        this.officeId = officeId;
+    }
+
+    public String getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(String areaId) {
+        this.areaId = areaId;
+    }
+}
