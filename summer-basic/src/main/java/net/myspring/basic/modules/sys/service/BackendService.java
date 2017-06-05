cc8be78d6ae666c4e5bac2672c0aa36826a8e87d
haos <1647424873@qq.com>
2017-06-05 11:43:50 +0800

    update

--- summer-basic/src/main/java/net/myspring/basic/modules/sys/service/BackendService.java
+++ summer-basic/src/main/java/net/myspring/basic/modules/sys/service/BackendService.java
@@ -65,7 +65,6 @@ public class BackendService {
         return backendDtoList;
     }
 
-    @Transactional
     public void logicDelete(String id) {
         backendRepository.logicDelete(id);
     }
