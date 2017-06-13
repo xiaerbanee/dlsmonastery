868acb85eace61b5bfd79f1e5c6699ec41598af2
wangzhangmao <wangcyh194520>
2017-06-13 14:40:22 +0800

    enum

--- ws-future/src/main/java/net/myspring/future/modules/basic/web/controller/DepotStoreController.java
+++ ws-future/src/main/java/net/myspring/future/modules/basic/web/controller/DepotStoreController.java
@@ -7,16 +7,20 @@ import net.myspring.common.enums.CompanyConfigCodeEnum;
 import net.myspring.common.response.RestResponse;
 import net.myspring.future.common.enums.*;
 import net.myspring.future.common.utils.RequestUtils;
+import net.myspring.future.modules.basic.client.OfficeClient;
+import net.myspring.future.modules.basic.domain.Depot;
 import net.myspring.future.modules.basic.domain.DepotStore;
 import net.myspring.future.modules.basic.dto.DepotReportDto;
 import net.myspring.future.modules.basic.dto.DepotShopDto;
 import net.myspring.future.modules.basic.dto.DepotStoreDto;
+import net.myspring.future.modules.basic.service.DepotService;
 import net.myspring.future.modules.basic.service.DepotShopService;
 import net.myspring.future.modules.basic.service.DepotStoreService;
 import net.myspring.future.modules.basic.web.form.DepotStoreForm;
 import net.myspring.future.modules.basic.web.query.DepotQuery;
 import net.myspring.future.modules.basic.web.query.DepotStoreQuery;
 import net.myspring.future.modules.crm.web.query.ReportQuery;
+import net.myspring.util.mapper.BeanUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,6 +30,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by liuj on 2017/5/12.
@@ -38,6 +43,10 @@ public class DepotStoreController {
     private DepotStoreService depotStoreService;
     @Autowired
     private RedisTemplate redisTemplate;
+    @Autowired
+    private OfficeClient officeClient;
+    @Autowired
+    private DepotService depotService;
 
     @RequestMapping(method = RequestMethod.GET)
     public Page<DepotStoreDto> list(Pageable pageable, DepotStoreQuery depotStoreQuery){
@@ -64,15 +73,24 @@ public class DepotStoreController {
         return new RestResponse("删除成功",null);
     }
 
-    @RequestMapping(value = "depotReportDate")
-    public List<DepotReportDto> depotReport(ReportQuery reportQuery){
-        List<DepotReportDto> list=depotStoreService.setReportData(reportQuery);
-        return list;
+    @RequestMapping(value = "storeReport")
+    public Page<DepotStoreDto> storeReport(Pageable pageable,ReportQuery reportQuery){
+        reportQuery.setOfficeIdList(officeClient.getOfficeFilterIds(RequestUtils.getRequestEntity().getOfficeId()));
+        reportQuery.setDepotIdList(depotService.filterDepotIds());
+        DepotStoreQuery depotStoreQuery = BeanUtil.map(reportQuery, DepotStoreQuery.class);
+        Page<DepotStoreDto> page = depotStoreService.findPage(pageable,depotStoreQuery);
+        depotStoreService.setReportData(page.getContent(),reportQuery);
+        return page;
     }
+
+    @RequestMapping(value = "storeReportDetail")
+    public Map<String,Integer> storeReportDetail(ReportQuery reportQuery){
+        return depotStoreService.getReportDetail(reportQuery);
+    }
+
 
     @RequestMapping(value = "getReportQuery")
     public ReportQuery getReportQuery(ReportQuery reportQuery){
-        reportQuery.getExtra().put("sumTypeList", SumTypeEnum.getList());
         reportQuery.getExtra().put("boolMap", BoolEnum.getMap());
         CompanyConfigCacheDto companyConfigCacheDto = CompanyConfigUtil.findByCode( redisTemplate, RequestUtils.getCompanyId(), CompanyConfigCodeEnum.PRODUCT_NAME.name());
         if(companyConfigCacheDto != null && "WZOPPO".equals(companyConfigCacheDto.getValue())) {
