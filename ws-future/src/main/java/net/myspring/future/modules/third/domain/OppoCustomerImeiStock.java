27866acf5611c7bab3250f0821f5cdcf9cdf91b1
guolm <625406542@qq.com>
2017-06-20 21:05:24 +0800

    工厂修改

---
+++ ws-future/src/main/java/net/myspring/future/modules/third/domain/OppoCustomerImeiStock.java
@@ -0,0 +1,55 @@
+package net.myspring.future.modules.third.domain;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by guolm on 2016/7/16.
+ */
+public class OppoCustomerImeiStock {
+
+    private String customerid;
+    private LocalDateTime date;
+    private String imei;
+    private String productcode;
+    private String transType;
+
+    public String getCustomerid() {
+        return customerid;
+    }
+
+    public void setCustomerid(String customerid) {
+        this.customerid = customerid;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getImei() {
+        return imei;
+    }
+
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
+
+    public String getProductcode() {
+        return productcode;
+    }
+
+    public void setProductcode(String productcode) {
+        this.productcode = productcode;
+    }
+
+    public String getTransType() {
+        return transType;
+    }
+
+    public void setTransType(String transType) {
+        this.transType = transType;
+    }
+}
