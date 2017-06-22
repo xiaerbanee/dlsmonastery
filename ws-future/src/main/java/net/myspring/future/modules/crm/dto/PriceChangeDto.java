00374a4cde685bada412333d15b5d1a4df07c67a
zhangyf <1054684157@qq.com>
2017-06-22 10:52:14 +0800

    update

--- ws-future/src/main/java/net/myspring/future/modules/crm/dto/PriceChangeDto.java
+++ ws-future/src/main/java/net/myspring/future/modules/crm/dto/PriceChangeDto.java
@@ -15,12 +15,12 @@ import java.util.List;
 public class PriceChangeDto extends DataDto<PriceChange> {
 
     private String name;
-    private String productTypeIds;
     private LocalDate priceChangeDate;
     private LocalDate uploadEndDate;
     private Integer checkPercent;
     private String status;
 
+    private String productTypeIds;
     private List<String> productTypeIdList;
     @CacheInput(inputKey = "productTypes",inputInstance = "productTypeIdList",outputInstance = "name")
     private List<String> productTypeNameList;
@@ -52,11 +52,7 @@ public class PriceChangeDto extends DataDto<PriceChange> {
     }
 
     public List<String> getProductTypeIdList() {
-        if(this.productTypeIds!=null){
-            return Arrays.asList(this.productTypeIds.split(CharConstant.COMMA));
-        }else{
-            return null;
-        }
+        return productTypeIdList;
     }
 
     public void setProductTypeIdList(List<String> productTypeIdList) {
