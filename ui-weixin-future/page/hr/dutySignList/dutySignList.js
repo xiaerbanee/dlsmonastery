ba51c935d6aaff71f8e83ded2985969889a6fd50
wangzhangmao <wangcyh194520>
2017-06-23 08:42:05 +0800

    menu

--- ui-weixin-future/page/hr/dutySignList/dutySignList.js
+++ ui-weixin-future/page/hr/dutySignList/dutySignList.js
@@ -1,156 +1,109 @@
-//获取应用实例
-var app = getApp();
-var $util = require("../../../util/util.js");
-Page({
-    data: {
-        page: {},
-        formData: {},
-        searchHidden: true,
-        activeItem: null
+//app.js
+var $util = require("/util/util.js");
+App({
+    onLaunch: function () {
     },
-    onLoad: function (option) {
+    getUserInfo: function (cb) {
+        var that = this
+        if (that.globalData.userInfo) {
+            typeof cb == "function" && cb(that.globalData.userInfo)
+        } else {
+            //调用登录接口
+            wx.login({
+                success: function (res) {
+                    wx.getUserInfo({
+                        success: function (res) {
+                            that.globalData.userInfo = res.userInfo
+                            typeof cb == "function" && cb(that.globalData.userInfo)
+                        }
+                    })
+                }
+            })
+        }
     },
-    onShow: function () {
+    globalData: {
+        sessionId: "",
+        userInfo: null,
+        weixinCode: null,
+        weixinAccount: null
+    },
+    //检查用户是否登陆，如果未登陆，自动登陆
+    autoLogin: function (cb) {
         var that = this;
-        app.autoLogin(function () {
-            that.initPage()
-        });
+        if (!that.globalData.weixinCode) {
+            that.getCode(function () {
+                that.checkLogin(cb);
+            })
+        } else {
+            that.checkLogin(cb);
+        }
+    }, checkLogin(cb) {
+        var that = this;
+        var token = wx.getStorageSync('token')
+        var distance = that.getDistance();
+        var reflushTokenDustance = wx.getStorageSync('token').expires_in;
+        //如果没有登陆
+        if (token == "" || $util.isNotBlank(token.access_token) || distance < 60 * 1000) {
+            that.getToken(cb);
+        } else if (distance < (reflushTokenDustance / 2) * 1000) {
+            that.getRefreshToken();
+        }
     },
-    initPage: function () {
+    getRefreshToken: function () {
+        var that = this;
+        var token = wx.getStorageSync('token');
+        var pages = getCurrentPages();
+        var currentPages = pages[pages.length - 1]
+        wx.request({
+            url: $util.getUrl('uaa/oauth/token?username=' + that.globalData.weixinCode + '&password=' + that.globalData.weixinCode + '&weixinCode=' + that.globalData.weixinCode + '&grant_type=refresh_token&refresh_token=' + token.refresh_token),
+            data: {},
+            method: 'POST',
+            success: function (res) {
+                if ($util.isNotBlank(res.data.access_token)) {
+                    res.data.exp = new Date().getTime();
+                    wx.setStorageSync('token', res.data);
+                    wx.navigateTo({
+                        url: '/' + currentPages.__route__
+                    });
+                }
+            }
+        })
+    },
+    getDistance: function () {
+        var that = this;
+        var token = wx.getStorageSync('token');
+        var distance;
+        var expDate = new Date(token.exp + token.expires_in * 1000);
+        return distance = expDate.getTime() - new Date().getTime();
+    }, getToken: function (cb) {
         var that = this;
         wx.request({
-            url: $util.getUrl("basic/hr/dutySign/getQuery"),
-            header: {
-                'x-auth-token': app.globalData.sessionId,
-                'authorization': "Bearer" + wx.getStorageSync('token').access_token
-            },
+            url: $util.getUrl('uaa/oauth/token?username=' + that.globalData.weixinCode + '&password=' + that.globalData.weixinCode + '&weixinCode=' + that.globalData.weixinCode + '&grant_type=password'),
+            data: {},
+            method: 'POST',
             success: function (res) {
-                that.setData({ formData: res.data });
-                that.pageRequest();
-            }
-        })
-    },
-    pageRequest: function () {
-        var that = this
-        wx.showToast({
-            title: '加载中',
-            icon: 'loading',
-            duration: 10000,
-            success: function (res) {
-                wx.request({
-                    url: $util.getUrl("basic/hr/dutySign"),
-                    header: {
-                        'x-auth-token': app.globalData.sessionId,
-                        'authorization': "Bearer" + wx.getStorageSync('token').access_token
-                    },
-                    data: $util.deleteExtra(that.data.formData),
-                    success: function (res) {
-
-                        that.setData({ page: res.data });
-                        wx.hideToast();
-                    }
-                })
-            }
-        })
-    },
-    add: function () {
-        wx.navigateTo({
-            url: '/page/hr/dutySignForm/dutySignForm?action=add'
-        })
-    },
-    search: function () {
-        var that = this;
-        that.setData({
-            searchHidden: !that.data.searchHidden
-        })
-    },
-    bindDateChange: function (e) {
-        var that = this;
-        var name = e.currentTarget.dataset.name;
-        if (name == 'dutyDateStart') {
-            that.setData({ "formData.dutyDateStart": e.detail.value });
-        } else {
-            that.setData({ "formData.dutyDateEnd": e.detail.value });
-        }
-    },
-    itemActive: function (e) {
-        var that = this;
-        var id = e.currentTarget.dataset.id;
-        for (var index in that.data.page.content) {
-            var item = that.data.page.content[index];
-            if (item.id == id) {
-                that.data.activeItem = item;
-                item.active = true;
-            } else {
-                item.active = false;
-            }
-        }
-        that.setData({ page: that.data.page });
-    },
-    showItemActionSheet: function (e) {
-        var that = this;
-        var id = e.currentTarget.dataset.id;
-        wx.showActionSheet({
-            itemList: ["详细", "删除"],
-            success: function (res) {
-                if (!res.cancel) {
-                    if (res.tapIndex == 0) {
-                        wx.navigateTo({
-                            url: '/page/hr/dutySignForm/dutySignForm?action=detail&id=' + id
-                        })
-                    } else if (res.tapIndex == 1) {
-                        wx.request({
-                            url: $util.getUrl("basic/hr/dutySign/delete"),
-                            data: { id: id },
-                            header: {
-                                'x-auth-token': app.globalData.sessionId,
-                                'authorization': "Bearer" + wx.getStorageSync('token').access_token
-                            },
-                            success: function (res) {
-                                that.pageRequest();
-                            }
-                        })
-                    }
+                if (res.statusCode == 401) {
+                    wx.navigateTo({
+                        url: '/page/sys/accountBind/accountBind'
+                    })
+                } else if ($util.isNotBlank(res.data.access_token)) {
+                    res.data.exp = new Date().getTime();
+                    wx.setStorageSync('token', res.data);
+                    typeof cb == "function" && cb();
                 }
             }
-        });
-    },
-    formSubmit: function (e) {
+        })
+    }, getCode(cb) {
         var that = this;
-        that.setData({ searchHidden: !that.data.searchHidden, formData: e.detail.value, "formData.page": 0 });
-        that.pageRequest();
-    },
-    toFirstPage: function () {
-        var that = this;
-        that.setData({ "formData.page": 0 });
-        that.pageRequest();
-    },
-    toPreviousPage: function () {
-        var that = this;
-        that.setData({ "formData.page": $util.getPreviousPageNumber(that.data.formData.page) });
-        that.pageRequest();
-    },
-    toNextPage: function () {
-        var that = this;
-        that.setData({ "formData.page": $util.getNextPageNumber(that.data.formData.page, that.data.page.totalPages) });
-        that.pageRequest();
-    },
-    toLastPage: function () {
-        var that = this;
-        that.setData({ "formData.page": that.data.page.totalPages - 1 });
-        that.pageRequest();
-    },
-    toPage: function () {
-        var that = this;
-        var itemList = $util.getPageList(that.data.formData.page, that.data.page.totalPages);
-        wx.showActionSheet({
-            itemList: itemList,
-            success: function (res) {
-                if (!res.cancel) {
-                    that.setData({ "formData.page": itemList[res.tapIndex] - 1 });
-                    that.pageRequest();
+        if (!that.globalData.weixinCode) {
+            wx.login({
+                success: function (loginRes) {
+                    if (loginRes.code) {
+                        that.globalData.weixinCode = loginRes.code;
+                        typeof cb == "function" && cb();
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 })
