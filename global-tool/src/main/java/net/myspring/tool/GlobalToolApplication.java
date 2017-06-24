af329b55eb6696b8f070bc0db4c4d21c9100f1d5
guolm <625406542@qq.com>
2017-06-24 14:36:12 +0800

    修改

--- global-tool/src/main/java/net/myspring/tool/GlobalToolApplication.java
+++ global-tool/src/main/java/net/myspring/tool/GlobalToolApplication.java
@@ -1,5 +1,6 @@
 package net.myspring.tool;
 
+import net.myspring.tool.common.repository.BaseRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -7,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
