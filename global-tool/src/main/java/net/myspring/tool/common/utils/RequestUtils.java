1c048ab973baf17e39a0a506ecb26f997e4c5541
liuj <287724593@qq.com>
2017-06-24 10:53:21 +0800

    update

--- global-tool/src/main/java/net/myspring/tool/common/utils/RequestUtils.java
+++ global-tool/src/main/java/net/myspring/tool/common/utils/RequestUtils.java
@@ -1,19 +1,8 @@
 package net.myspring.tool.common.utils;
 
-import com.google.common.collect.Maps;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
-import net.myspring.util.json.ObjectMapperUtils;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.jwt.Jwt;
-import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -21,19 +10,8 @@ import java.util.Map;
  * Created by liuj on 2017/4/2.
  */
 public class RequestUtils {
-    public static final String REQEUST_ENTITY = "requestEntity";
-
     public static RequestEntity getRequestEntity() {
-        HttpServletRequest request=null;
-        if(RequestContextHolder.getRequestAttributes()!=null){
-            request  = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        }
-        RequestEntity requestEntity;
-        if(request!=null&&request.getAttribute(REQEUST_ENTITY) != null) {
-            requestEntity = (RequestEntity) request.getAttribute(REQEUST_ENTITY);
-        } else {
-            requestEntity = new RequestEntity();
-        }
+        RequestEntity  requestEntity = new RequestEntity();
         Map<String,String> securityMap = getSecurityMap();
         if(securityMap.size()>0) {
             requestEntity.setAccountId(securityMap.get("accountId"));
@@ -41,9 +19,6 @@ public class RequestUtils {
             requestEntity.setPositionId(securityMap.get("positionId"));
             requestEntity.setOfficeId(securityMap.get("officeId"));
             requestEntity.setEmployeeId(securityMap.get("employeeId"));
-        }
-        if(request!=null){
-            request.setAttribute(REQEUST_ENTITY,requestEntity);
         }
         return requestEntity;
     }
--- summer-basic/src/main/java/net/myspring/basic/common/utils/RequestUtils.java
+++ summer-basic/src/main/java/net/myspring/basic/common/utils/RequestUtils.java
@@ -1,18 +1,8 @@
 package net.myspring.basic.common.utils;
 
-import com.google.common.collect.Maps;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
-import net.myspring.util.json.ObjectMapperUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.jwt.Jwt;
-import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -20,16 +10,8 @@ import java.util.Map;
  * Created by liuj on 2017/4/2.
  */
 public class RequestUtils {
-    public static final String REQEUST_ENTITY = "requestEntity";
-
     public static RequestEntity getRequestEntity() {
-        HttpServletRequest request  = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        RequestEntity requestEntity;
-        if(request.getAttribute(REQEUST_ENTITY) != null) {
-            requestEntity = (RequestEntity) request.getAttribute(REQEUST_ENTITY);
-        } else {
-            requestEntity = new RequestEntity();
-        }
+        RequestEntity  requestEntity = new RequestEntity();
         Map<String,String> securityMap = getSecurityMap();
         if(securityMap.size()>0) {
             requestEntity.setAccountId(securityMap.get("accountId"));
@@ -37,9 +19,7 @@ public class RequestUtils {
             requestEntity.setPositionId(securityMap.get("positionId"));
             requestEntity.setOfficeId(securityMap.get("officeId"));
             requestEntity.setEmployeeId(securityMap.get("employeeId"));
-            requestEntity.setCompanyName(securityMap.get("companyName"));
         }
-        request.setAttribute(REQEUST_ENTITY,requestEntity);
         return requestEntity;
     }
 
@@ -50,6 +30,7 @@ public class RequestUtils {
     public static String getCompanyId() {
         return getRequestEntity().getCompanyId();
     }
+
 
     private  static Map<String, String> getSecurityMap() {
         OAuth2Authentication auth = (OAuth2Authentication)SecurityContextHolder.getContext().getAuthentication();
