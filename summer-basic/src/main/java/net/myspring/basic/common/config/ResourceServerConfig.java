8c3526e78b72ad7b250140ac73344bd3585cc3e3
liuj <287724593@qq.com>
2017-06-29 13:17:29 +0800

    update

--- summer-basic/src/main/java/net/myspring/basic/common/config/ResourceServerConfig.java
+++ summer-basic/src/main/java/net/myspring/basic/common/config/ResourceServerConfig.java
@@ -4,6 +4,7 @@ import feign.RequestInterceptor;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
@@ -17,6 +18,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
  */
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Bean
     public RequestInterceptor oauth2FeignRequestInterceptor(OAuth2ClientContext oauth2ClientContext, ClientCredentialsResourceDetails resource) {
