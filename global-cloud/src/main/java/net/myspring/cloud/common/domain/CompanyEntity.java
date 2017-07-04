5ff4f790f6f167061b5c11bcdd6f18976e0fc265
lihx <lixh@qq.com>
2017-07-04 18:27:26 +0800

    update

--- global-cloud/src/main/java/net/myspring/cloud/common/domain/CompanyEntity.java
+++ global-cloud/src/main/java/net/myspring/cloud/common/domain/CompanyEntity.java
@@ -2,11 +2,8 @@ package net.myspring.cloud.common.domain;
 
 
 import net.myspring.cloud.common.utils.RequestUtils;
-import org.apache.commons.lang.StringUtils;
 
-import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.PrePersist;
 
 /**
  * Created by liuj on 2017/4/12.
