2e1efe5adebde1f158ace425032fb4c02ebeaf04
zhucc <1125517699@qq.com>
2017-07-04 22:00:52 +0800

    update

--- ws-future/src/main/java/net/myspring/future/modules/crm/service/AfterSaleStoreAllotService.java
+++ ws-future/src/main/java/net/myspring/future/modules/crm/service/AfterSaleStoreAllotService.java
@@ -1,17 +1,29 @@
 package net.myspring.future.modules.crm.service;
 
+import com.google.common.collect.Lists;
 import net.myspring.future.common.utils.CacheUtils;
+import net.myspring.future.modules.basic.dto.DepotShopDto;
+import net.myspring.future.modules.basic.web.query.DepotShopQuery;
 import net.myspring.future.modules.crm.dto.AfterSaleProductAllotDto;
 import net.myspring.future.modules.crm.dto.AfterSaleStoreAllotDto;
 import net.myspring.future.modules.crm.repository.AfterSaleProductAllotRepository;
 import net.myspring.future.modules.crm.repository.AfterSaleStoreAllotRepository;
 import net.myspring.future.modules.crm.web.query.AfterSaleProductAllotQuery;
 import net.myspring.future.modules.crm.web.query.AfterSaleStoreAllotQuery;
+import net.myspring.util.excel.ExcelUtils;
+import net.myspring.util.excel.SimpleExcelBook;
+import net.myspring.util.excel.SimpleExcelColumn;
+import net.myspring.util.excel.SimpleExcelSheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by zhucc on 2017/7/4.
@@ -29,5 +41,25 @@ public class AfterSaleStoreAllotService {
         Page<AfterSaleStoreAllotDto> afterSaleStoreAllotDtoPage=afterSaleStoreAllotRepository.findPage(pageable,afterSaleStoreAllotQuery);
         cacheUtils.initCacheInput(afterSaleStoreAllotDtoPage.getContent());
         return afterSaleStoreAllotDtoPage;
+    }
+
+    public SimpleExcelBook findSimpleExcelSheet(AfterSaleStoreAllotQuery afterSaleStoreAllotQuery)  {
+        Workbook workbook = new SXSSFWorkbook(10000);
+        List<AfterSaleStoreAllotDto> afterSaleStoreAllotDtos = afterSaleStoreAllotRepository.findFilter(afterSaleStoreAllotQuery);
+        cacheUtils.initCacheInput(afterSaleStoreAllotDtos);
+        List<SimpleExcelColumn> simpleExcelColumnList= Lists.newArrayList();
+
+        simpleExcelColumnList.add(new SimpleExcelColumn(workbook,"businessId","售后单号"));
+        simpleExcelColumnList.add(new SimpleExcelColumn(workbook,"productName","货品"));
+        simpleExcelColumnList.add(new SimpleExcelColumn(workbook,"fromStoreName","调出仓库"));
+        simpleExcelColumnList.add(new SimpleExcelColumn(workbook,"toStoreName","调后仓库"));
+        simpleExcelColumnList.add(new SimpleExcelColumn(workbook,"outCode","财务编号"));
+        simpleExcelColumnList.add(new SimpleExcelColumn(workbook,"createdByName","创建人"));
+        simpleExcelColumnList.add(new SimpleExcelColumn(workbook,"createdDate","创建时间"));
+
+        SimpleExcelSheet simpleExcelSheet = new SimpleExcelSheet("售后调拨",afterSaleStoreAllotDtos,simpleExcelColumnList);
+        ExcelUtils.doWrite(workbook,simpleExcelSheet);
+        SimpleExcelBook simpleExcelBook = new SimpleExcelBook(workbook,"售后调拨"+ UUID.randomUUID()+".xlsx",simpleExcelSheet);
+        return simpleExcelBook;
     }
 }
