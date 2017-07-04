618af82ff09d2a458e7af32e59c609609889e2ae
lihx <lixh@qq.com>
2017-07-04 11:01:13 +0800

    update

--- global-cloud/src/main/java/net/myspring/cloud/modules/kingdee/service/BdFlexItemGroupService.java
+++ global-cloud/src/main/java/net/myspring/cloud/modules/kingdee/service/BdFlexItemGroupService.java
@@ -1,23 +1,14 @@
 package net.myspring.cloud.modules.kingdee.service;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import net.myspring.cloud.common.dataSource.annotation.KingdeeDataSource;
-import net.myspring.cloud.modules.kingdee.domain.BdAccount;
 import net.myspring.cloud.modules.kingdee.domain.BdFlexItemGroup;
-import net.myspring.cloud.modules.kingdee.domain.BdFlexItemProperty;
 import net.myspring.cloud.modules.kingdee.repository.BdFlexItemGroupRepository;
-import net.myspring.cloud.modules.sys.dto.VoucherDto;
-import net.myspring.cloud.modules.sys.dto.VoucherModel;
-import net.myspring.common.constant.CharConstant;
-import net.myspring.util.text.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * 核算维度组
@@ -25,7 +16,7 @@ import java.util.stream.Collectors;
  */
 @Service
 @KingdeeDataSource
-@Transactional
+@Transactional(readOnly = true)
 public class BdFlexItemGroupService {
     @Autowired
     private BdFlexItemGroupRepository bdFlexItemGroupRepository;
