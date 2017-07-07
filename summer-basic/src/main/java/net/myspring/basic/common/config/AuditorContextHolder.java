f2f8e42407615d5eeaae3121d01afd975cd4662c
liuj <287724593@qq.com>
2017-07-07 11:20:18 +0800

    auditor config

---
+++ summer-basic/src/main/java/net/myspring/basic/common/config/AuditorContextHolder.java
@@ -0,0 +1,33 @@
+package net.myspring.basic.common.config;
+
+import net.myspring.basic.common.utils.RequestUtils;
+import org.apache.commons.lang.StringUtils;
+
+public class AuditorContextHolder {
+    private String accountId;
+
+    private static ThreadLocal<AuditorContextHolder> threadLocal = new ThreadLocal<AuditorContextHolder>();
+
+    public static AuditorContextHolder get() {
+        if (threadLocal.get() == null) {
+            AuditorContextHolder auditorContextHolder = new AuditorContextHolder();
+            threadLocal.set(auditorContextHolder);
+        }
+        return threadLocal.get();
+    }
+
+    public void remove() {
+        threadLocal.remove();
+    }
+
+    public String getAccountId() {
+        if(StringUtils.isBlank(accountId)) {
+            accountId = RequestUtils.getAccountId();
+        }
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+}
