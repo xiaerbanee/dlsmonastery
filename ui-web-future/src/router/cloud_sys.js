328e0c6de95c0509c65b3f56567c9ebebf2a8d8f
lihx <lixh@qq.com>
2017-07-08 18:01:37 +0800

    currentKingdeeBookForm

--- ui-web-future/src/router/cloud_sys.js
+++ ui-web-future/src/router/cloud_sys.js
@@ -2,6 +2,7 @@ const kingdeeBookList  = r => require.ensure([], () => r(require('pages/cloud/sy
 const kingdeeBookForm  = r => require.ensure([], () => r(require('pages/cloud/sys/kingdeeBookForm.vue')));
 const accountKingdeeBookList  = r => require.ensure([], () => r(require('pages/cloud/sys/accountKingdeeBookList.vue')));
 const accountKingdeeBookForm  = r => require.ensure([], () => r(require('pages/cloud/sys/accountKingdeeBookForm.vue')));
+const currentKingdeeBookForm  = r => require.ensure([], () => r(require('pages/cloud/sys/currentKingdeeBookForm.vue')));
 const materialPriceManager  = r => require.ensure([], () => r(require('pages/cloud/sys/materialPriceManager.vue')));
 const voucherList  = r => require.ensure([], () => r(require('pages/cloud/sys/voucherList.vue')));
 const voucherForm  = r => require.ensure([], () => r(require('pages/cloud/sys/voucherForm.vue')));
@@ -14,6 +15,7 @@ let routes = [
   {path: '/cloud/sys/kingdeeBookForm',component: kingdeeBookForm,name: 'kingdeeBookForm',meta:{menu:"kingdeeBookList",keepAlive:true}},
   {path: '/cloud/sys/accountKingdeeBookList',component: accountKingdeeBookList,name: 'accountKingdeeBookList'},
   {path: '/cloud/sys/accountKingdeeBookForm',component: accountKingdeeBookForm,name: 'accountKingdeeBookForm',meta:{menu:"accountKingdeeBookList"}},
+  {path: '/cloud/sys/currentKingdeeBookForm',component: currentKingdeeBookForm,name: 'currentKingdeeBookForm'},
   {path: '/cloud/sys/materialPriceManager',component: materialPriceManager,name: 'materialPriceManager'},
   {path: '/cloud/sys/voucherList',component: voucherList,name: 'voucherList'},
   {path: '/cloud/sys/voucherForm',component: voucherForm,name: 'voucherForm',meta:{menu:"voucherList"}},
