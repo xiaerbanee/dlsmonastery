b135fadc5f4aa6d7d6e6f2f839a06e0e454f9391
guolm <625406542@qq.com>
2017-07-11 15:59:05 +0800

    task修改

---
+++ global-task/src/main/java/net/myspring/task/tool/client/OppoClient.java
@@ -0,0 +1,17 @@
+package net.myspring.task.tool.client;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+/**
+ * Created by guolm on 2017/6/11.
+ */
+
+@FeignClient("ws-future")
+public interface OppoClient {
+
+}
