4e63be39b16019cb2f73adc9b2e46ef6e87ee132
guolm <625406542@qq.com>
2017-07-12 18:55:23 +0800

    工厂修改

--- global-task/src/main/java/net/myspring/task/tool/utils/OppoPushScheduleUtils.java
+++ global-task/src/main/java/net/myspring/task/tool/utils/OppoPushScheduleUtils.java
@@ -1,8 +1,28 @@
 package net.myspring.task.tool.utils;
 
+import net.myspring.task.tool.client.FactoryClient;
+import net.myspring.util.time.LocalDateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class OppoPushScheduleUtils {
 
+    @Autowired
+    private FactoryClient factoryClient;
+
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Scheduled(cron = "0 0 22,23 * * ?")
+    public void pullOppoData(){
+        logger.info("工厂上抛数据开始");
+        String date= LocalDateUtils.format(LocalDate.now());
+        factoryClient.pushOppoFactoryData(date);
+        logger.info("工厂上抛数据结束");
+    }
 }
