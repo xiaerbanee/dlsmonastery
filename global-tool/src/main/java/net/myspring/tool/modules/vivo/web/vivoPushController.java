9980028cf4c63d6f88aeb10d9013158dde224bf7
sungm <360377289@qq.com>
2017-07-14 12:50:27 +0800

    vivo工厂数据上抛

--- global-tool/src/main/java/net/myspring/tool/modules/vivo/web/vivoPushController.java
+++ global-tool/src/main/java/net/myspring/tool/modules/vivo/web/vivoPushController.java
@@ -1,17 +1,18 @@
 package net.myspring.tool.modules.vivo.web;
 
 import net.myspring.tool.common.dataSource.DbContextHolder;
-import net.myspring.tool.modules.vivo.domain.SCustomersM13e00;
 import net.myspring.tool.modules.vivo.dto.SCustomerDto;
+import net.myspring.tool.modules.vivo.dto.SPlantCustomerStockDto;
 import net.myspring.tool.modules.vivo.service.VivoPushService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping(value = "vivo")
+@RequestMapping(value = "factory/vivo")
 public class vivoPushController {
     @Autowired
     private VivoPushService vivoPushService;
@@ -26,5 +27,12 @@ public class vivoPushController {
         List<SCustomerDto> futureCustomerDtoList = vivoPushService.getVivoCustomersData(date);
         DbContextHolder.get().setCompanyName("JXVIVO");
         vivoPushService.saveVivoPushSCustomers(futureCustomerDtoList,date);
+        //库存汇总数据
+        DbContextHolder.get().setCompanyName("JXOPPO");
+        List<SPlantCustomerStockDto> sPlantCustomerStockDtoList = vivoPushService.getCustomerStockData(date);
+        System.err.println("DbContextHolder=="+DbContextHolder.get().getCompanyName()+"\t"+DbContextHolder.get().getDataSourceType());
+        DbContextHolder.get().setCompanyName("JXVIVO");
+        Map<String,String> productColorMap = vivoPushService.getProductColorMap();
+        vivoPushService.pushCustomerStockData(sPlantCustomerStockDtoList,productColorMap,date);
     }
 }
