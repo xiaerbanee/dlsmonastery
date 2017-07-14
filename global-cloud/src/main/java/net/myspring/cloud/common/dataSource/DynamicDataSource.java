c2014801cdf4846027841961bac95ae6c96c8c8f
liuj <287724593@qq.com>
2017-07-14 16:58:03 +0800

    update

--- global-cloud/src/main/java/net/myspring/cloud/common/dataSource/DynamicDataSource.java
+++ global-cloud/src/main/java/net/myspring/cloud/common/dataSource/DynamicDataSource.java
@@ -1,8 +1,5 @@
 package net.myspring.cloud.common.dataSource;
 
-import net.myspring.cloud.common.enums.DataSourceTypeEnum;
-import net.myspring.cloud.common.utils.RequestUtils;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 public class DynamicDataSource extends AbstractRoutingDataSource {
 
