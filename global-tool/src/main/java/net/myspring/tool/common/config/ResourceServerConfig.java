e47687fdea5e845bbb4254f9b2e7edb460db0de0
liuj <287724593@qq.com>
2017-07-17 15:58:33 +0800

    task

--- global-tool/src/main/java/net/myspring/tool/common/config/ResourceServerConfig.java
+++ global-tool/src/main/java/net/myspring/tool/common/config/ResourceServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequest
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -30,5 +31,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Bean
     public RequestInterceptor oauth2FeignRequestInterceptor(OAuth2ClientContext oAuth2ClientContext) {
         return new OAuth2FeignRequestInterceptor(oAuth2ClientContext,resource());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/factory/**").permitAll().anyRequest().authenticated();
     }
 }
