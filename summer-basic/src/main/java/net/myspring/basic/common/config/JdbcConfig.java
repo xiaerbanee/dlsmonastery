8e0eaa0302cce7cb2abfc6ce850d48c7f36f3694
liuj <287724593@qq.com>
2017-07-19 16:19:09 +0800

    多数据源切换

--- summer-basic/src/main/java/net/myspring/basic/common/config/JdbcConfig.java
+++ summer-basic/src/main/java/net/myspring/basic/common/config/JdbcConfig.java
@@ -1,6 +1,7 @@
 package net.myspring.basic.common.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import net.myspring.basic.SummerBasicApplication;
 import net.myspring.basic.common.datasource.DynamicDataSource;
@@ -45,6 +46,7 @@ public class JdbcConfig {
         }
         DynamicDataSource dataSource = new DynamicDataSource();
         dataSource.setTargetDataSources(targetDataSources);
+        dataSource.setDefaultTargetDataSource(Lists.newArrayList(targetDataSources).get(0));
         return dataSource;
     }
 
--- ws-future/src/main/java/net/myspring/future/common/config/JdbcConfig.java
+++ ws-future/src/main/java/net/myspring/future/common/config/JdbcConfig.java
@@ -1,6 +1,7 @@
 package net.myspring.future.common.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import net.myspring.common.enums.CompanyNameEnum;
 import net.myspring.future.WsFutureApplication;
@@ -45,6 +46,7 @@ public class JdbcConfig {
         }
         DynamicDataSource dataSource = new DynamicDataSource();
         dataSource.setTargetDataSources(targetDataSources);
+        dataSource.setDefaultTargetDataSource(Lists.newArrayList(targetDataSources).get(0));
         return dataSource;
     }
 
