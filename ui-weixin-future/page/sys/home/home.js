791d5a0ecb7072d7516a850c6112d7eff274889e
zhucc <1125517699@qq.com>
2017-07-20 17:27:21 +0800

    update

--- ui-weixin-future/page/sys/home/home.js
+++ ui-weixin-future/page/sys/home/home.js
@@ -5,15 +5,26 @@ Page({
     data: {
         weixinAccounts: null,
         weixinAccountsHidden: true,
+        companyHidden: false,
         menuList: null,
-        tip:""
+        tip:"",
+        companyNames:[],
     },
     onLoad: function (options) {
-
+      let that=this;
+      wx.request({
+        url: $util.getUaaUrl('/user/getCompanyNames'),
+        header: {
+          Cookie: "JSESSIONID=" + app.globalData.sessionId
+        },
+        success:function(res){
+          that.setData({companyNames:res.data})
+        }
+      })
     },
     onShow: function (options) {
         var that = this;
-        if (that.data.weixinAccountsHidden) {
+        if ($util.isNotBlank(app.globalData.companyName)) {
             app.autoLogin(function () {
                 that.initPage();
             })
@@ -23,7 +34,6 @@ Page({
         if (app.globalData.weixinAccounts.length > 1 && app.globalData.weixinAccount == null) {
             that.setData({ weixinAccountsHidden: false, weixinAccounts: app.globalData.weixinAccounts })
         } else {
-          console.log(">>>>>",app.globalData.weixinAccount)
           that.setData({ tip: "当前登录帐号为：" + app.globalData.weixinAccount.loginName })
 
             that.setData({ weixinAccountsHidden: true })
@@ -40,7 +50,6 @@ Page({
         if (app.globalData.weixinAccount.accountName==='xcxtest'){
           that.setData({tip: "当前登录的为测试帐号xcxtest，请绑定并登陆自己的真实帐号"})
         }else{
-          console.log("++++", app.globalData.weixinAccount)
           that.setData({ tip: "当前登录帐号为：" + app.globalData.weixinAccount.accountName})
         }
     },
@@ -49,12 +58,14 @@ Page({
         wx.request({
             url: $util.getUaaUrl('/user/logout'),
             method: 'POST',
+            data: { companyName: app.globalData.companyName},
             header: {
                 Cookie: "JSESSIONID=" + app.globalData.sessionId
             },
             success: function () {
                 wx.request({
                     url: $util.getUaaUrl('/logout'),
+                    data: { companyName: app.globalData.companyName },
                     method: 'POST',
                     header: {
                         Cookie: "JSESSIONID=" + app.globalData.sessionId
@@ -66,7 +77,7 @@ Page({
                         that.setData({ weixinAccountsHidden: false })
                         wx.request({
                             url: $util.getUaaUrl('user/getWeixinAccounts?weixinCode=' + app.globalData.weixinCode),
-                            data: {},
+                            data: { companyName: app.globalData.companyName},
                             method: 'POST',
                             success: function (res) {
                                 app.globalData.weixinAccounts = res.data;
@@ -78,4 +89,14 @@ Page({
             }
         })
     },
+    chooseCompany:function(e){
+      app.globalData.companyName =  e.currentTarget.dataset.name;
+      var that = this;
+      if ($util.isNotBlank(app.globalData.companyName)) {
+        that.setData({ companyHidden: true })
+        app.autoLogin(function () {
+          that.initPage();
+        })
+      }
+    }
 })
