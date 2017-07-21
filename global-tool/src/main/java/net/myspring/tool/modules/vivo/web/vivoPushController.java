f402372ed504ad4cc0caa92eb43e855e1fe13757
liuj <287724593@qq.com>
2017-07-21 18:40:33 +0800

    update

--- global-tool/src/main/java/net/myspring/tool/modules/vivo/web/vivoPushController.java
+++ global-tool/src/main/java/net/myspring/tool/modules/vivo/web/vivoPushController.java
@@ -2,12 +2,10 @@ package net.myspring.tool.modules.vivo.web;
 
 import net.myspring.tool.common.dataSource.DbContextHolder;
 import net.myspring.tool.modules.vivo.domain.SProductItemLend;
-import net.myspring.tool.modules.vivo.domain.SStores;
 import net.myspring.tool.modules.vivo.dto.SCustomerDto;
 import net.myspring.tool.modules.vivo.dto.SPlantCustomerStockDetailDto;
 import net.myspring.tool.modules.vivo.dto.SPlantCustomerStockDto;
 import net.myspring.tool.modules.vivo.dto.VivoCustomerSaleImeiDto;
-import net.myspring.tool.modules.vivo.service.IDvivoPushService;
 import net.myspring.tool.modules.vivo.service.VivoPushService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +20,6 @@ public class vivoPushController {
 
     @Autowired
     private VivoPushService vivoPushService;
-    @Autowired
-    private IDvivoPushService idvivoPushService;
 
     @RequestMapping(value = "pushVivoData")
     public void pushVivoData(String date){
@@ -48,30 +44,6 @@ public class vivoPushController {
         vivoPushService.pushProductImeSaleData(vivoCustomerSaleImeiDtoList,productColorMap,date);
         //一代仓库上抛
         vivoPushService.pushSStoreData();
-    }
-
-
-    @RequestMapping(value = "pushIDvivoData")
-    public void pushIDvivoData(String date){
-        //同步机构数据
-        DbContextHolder.get().setCompanyName("IDVIVO");
-        idvivoPushService.pushIDvivoZonesData();
-        //同步客户数据
-        List<SCustomerDto> sCustomerDtos=idvivoPushService.getVivoCustomersData(date);
-        idvivoPushService.pushIDVivoSCustomersData(sCustomerDtos);
-        //同步库存数据
-        Map<String,String> productColorMap = idvivoPushService.getProductColorMap();
-        List<SPlantCustomerStockDto> sPlantCustomerStockDtoList = idvivoPushService.getCustomerStockData(date);
-        idvivoPushService.pushCustomerStockData(sPlantCustomerStockDtoList,productColorMap,date);
-        //同步库存明细数据
-        List<SPlantCustomerStockDetailDto>  sPlantCustomerStockDetailDtoList=idvivoPushService.getCustomerStockDetailData(date);
-        idvivoPushService.pushCustomerStockDetailData(sPlantCustomerStockDetailDtoList,productColorMap,date);
-        //同步仓库数据
-        List<SStores> sStoresList=idvivoPushService.getCustomerStoreData();
-        idvivoPushService.pushCustomerStoresData(sStoresList);
-        //同步核销数据
-        List<VivoCustomerSaleImeiDto> vivoCustomerSaleImeiDtoList = idvivoPushService.getProductImeSaleData(date);
-        idvivoPushService.pushProductImeSaleData(vivoCustomerSaleImeiDtoList,productColorMap,date);
     }
 
 }
