3149045e62880e9e5744dfef2dffd341fc11ac80
zhucc <1125517699@qq.com>
2017-07-26 15:16:36 +0800

    update

---
+++ ui-web-future/src/locate/basic_salary_zh-CN.js
@@ -0,0 +1,25 @@
+export default{
+  salaryList: {
+    salaryList: '我的工资',
+    add: "添加",
+    query: "查询",
+    officeName: "部门",
+    employeeName: "姓名",
+    month: "月份",
+    productName: "项目",
+    productValue: "值",
+    loading: "拼命加载中"
+  },
+  salaryForm:{
+    salaryForm:"工资条添加",
+    exportTemplate: "导入模板",
+    exportData: "导入数据",
+    yearMonth: "月份",
+    remarks: "备注",
+    selectMonth: "选择月份",
+    clickUpload: "点击上传",
+    uploadImageSizeFor5000KB: "只能上传图片文件，且不超过500kb",
+    prerequisiteMessage: "必填信息",
+
+  }
+}
