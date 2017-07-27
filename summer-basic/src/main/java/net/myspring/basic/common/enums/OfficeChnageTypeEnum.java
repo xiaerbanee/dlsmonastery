92b5fad0acf8089b0afc886494c1bce1bfe32d75
zhucc <1125517699@qq.com>
2017-07-27 17:39:54 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	summer-gateway.log.1

--- summer-basic/src/main/java/net/myspring/basic/common/enums/OfficeChnageTypeEnum.java
+++ summer-basic/src/main/java/net/myspring/basic/common/enums/OfficeChnageTypeEnum.java
@@ -1,6 +1,21 @@
 package net.myspring.basic.common.enums;
 
+import com.google.common.collect.Lists;
+import net.myspring.util.collection.CollectionUtil;
+
+import java.util.List;
+
 public enum  OfficeChnageTypeEnum {
 
     上级,任务点位,名称;
+    private static List<String> list= Lists.newArrayList();
+
+    public static List<String> getList(){
+        if(CollectionUtil.isEmpty(list)){
+            for(OfficeChnageTypeEnum officeChnageTypeEnum:OfficeChnageTypeEnum.values()){
+                list.add(officeChnageTypeEnum.name());
+            }
+        }
+        return list;
+    }
 }
