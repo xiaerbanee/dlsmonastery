2091b58c19f673c47622b7e8b9d00f5b3eb532f4
qianl <1141185821@qq.com>
2017-07-27 18:30:01 +0800

    build

--- ws-future/src/main/java/net/myspring/future/modules/basic/web/controller/SimpleProcessController.java
+++ ws-future/src/main/java/net/myspring/future/modules/basic/web/controller/SimpleProcessController.java
@@ -2,10 +2,12 @@ package net.myspring.future.modules.basic.web.controller;
 
 import net.myspring.future.modules.basic.dto.SimpleProcessDetailDto;
 import net.myspring.future.modules.basic.service.SimpleProcessService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,6 +19,9 @@ public class SimpleProcessController {
 
     @RequestMapping(value = "findBySimpleProcessId")
     public List<SimpleProcessDetailDto> findBySimpleProcessId(String simpleProcessId){
+        if(StringUtils.isBlank(simpleProcessId)){
+            return new ArrayList<>();
+        }
         return processDetailService.findBySimpleProcessId(simpleProcessId);
     }
 }
