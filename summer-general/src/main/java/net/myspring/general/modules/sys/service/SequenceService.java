0f7dcb7b56bd66e85f6f58d265880d7c2fc56712
qianl <1141185821@qq.com>
2017-08-09 09:38:24 +0800

    build

--- summer-general/src/main/java/net/myspring/general/modules/sys/service/SequenceService.java
+++ summer-general/src/main/java/net/myspring/general/modules/sys/service/SequenceService.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
