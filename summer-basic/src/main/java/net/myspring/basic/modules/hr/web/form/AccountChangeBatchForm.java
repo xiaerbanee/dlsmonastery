78a4ae70a871b4875e280b93d6e4be8abe0910f2
wangzm <wangzm@12456>
2017-08-09 14:48:54 +0800

    employee_depot

---
+++ summer-basic/src/main/java/net/myspring/basic/modules/hr/web/form/AccountChangeBatchForm.java
@@ -0,0 +1,40 @@
+package net.myspring.basic.modules.hr.web.form;
+
+public class AccountChangeBatchForm {
+    private String loginName;
+    private String newValue;
+    private String type;
+    private String remarks;
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getNewValue() {
+        return newValue;
+    }
+
+    public void setNewValue(String newValue) {
+        this.newValue = newValue;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+}
