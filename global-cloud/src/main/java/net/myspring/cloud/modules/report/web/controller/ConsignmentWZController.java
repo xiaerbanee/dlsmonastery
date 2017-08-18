9b4dab8b637b84b480f96519f3a56cf44e44641c
lihx <lixh@qq.com>
2017-08-18 14:48:02 +0800

    update

--- global-cloud/src/main/java/net/myspring/cloud/modules/report/web/controller/ConsignmentWZController.java
+++ global-cloud/src/main/java/net/myspring/cloud/modules/report/web/controller/ConsignmentWZController.java
@@ -1,8 +1,11 @@
 package net.myspring.cloud.modules.report.web.controller;
 
+import net.myspring.cloud.common.enums.KingdeeNameEnum;
+import net.myspring.cloud.common.utils.RequestUtils;
 import net.myspring.cloud.modules.report.dto.ConsignmentDto;
 import net.myspring.cloud.modules.report.service.ConsignmentWZService;
 import net.myspring.cloud.modules.report.web.query.ConsignmentWZQuery;
+import net.myspring.common.exception.ServiceException;
 import net.myspring.util.excel.ExcelView;
 import net.myspring.util.time.LocalDateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,14 +27,23 @@ public class ConsignmentWZController {
 
     @RequestMapping(value = "list")
     public List<ConsignmentDto> list (ConsignmentWZQuery consignmentWZQuery){
-        return consignmentWZService.findConsignmentReport(consignmentWZQuery);
+        if (KingdeeNameEnum.WZOPPO.name().equals(RequestUtils.getCompanyName())) {
+            return consignmentWZService.findConsignmentReport(consignmentWZQuery);
+        }else {
+            throw new ServiceException("该报表只能用于温州（WZOPPO）账套,请确保当前使用的为温州账套");
+        }
     }
 
     @RequestMapping(value = "export")
     public ModelAndView export (String dateStart, String dateEnd ){
-        if (dateStart != null && dateEnd != null) {
-            return new ModelAndView(new ExcelView(), "simpleExcelBook", consignmentWZService.export(LocalDateUtils.parse(dateStart),LocalDateUtils.parse(dateEnd)));
+        if (KingdeeNameEnum.WZOPPO.name().equals(RequestUtils.getCompanyName())) {
+            if (dateStart != null && dateEnd != null) {
+                return new ModelAndView(new ExcelView(), "simpleExcelBook", consignmentWZService.export(LocalDateUtils.parse(dateStart),LocalDateUtils.parse(dateEnd)));
+            }else{
+                throw new ServiceException("时间不能为空！");
+            }
+        }else {
+            throw new ServiceException("该报表只能用于温州（WZOPPO）账套,请确保当前使用的为温州账套");
         }
-        return null;
     }
 }
