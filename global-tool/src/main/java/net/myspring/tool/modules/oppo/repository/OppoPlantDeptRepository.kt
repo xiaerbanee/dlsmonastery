d2925b45a83dfd096ddc6159fb9e287c309fa646
sungm <360377289@qq.com>
2017-08-19 16:38:49 +0800

    Oppo:体验店接口

--- global-tool/src/main/java/net/myspring/tool/modules/oppo/repository/OppoPlantDeptRepository.kt
+++ global-tool/src/main/java/net/myspring/tool/modules/oppo/repository/OppoPlantDeptRepository.kt
@@ -2,7 +2,17 @@ package net.myspring.tool.modules.oppo.repository
 
 import net.myspring.tool.common.repository.BaseRepository
 import net.myspring.tool.modules.oppo.domain.OppoPlantDept
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 
-interface OppoPlantDeptRepository :BaseRepository<OppoPlantDept,String>{
+interface OppoPlantDeptRepository :BaseRepository<OppoPlantDept,String>,OppoPlantDeptRepositoryCustom{
+
+}
+
+interface OppoPlantDeptRepositoryCustom{
+
+}
+
+class OppoPlantDeptRepositoryImpl @Autowired constructor(val namedParameterJdbcTemplate: NamedParameterJdbcTemplate):OppoPlantDeptRepositoryCustom{
 
 }
