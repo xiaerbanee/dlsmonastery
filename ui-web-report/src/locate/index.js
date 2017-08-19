e6fa63070376118ea1d08258b4cc7987842ddbfd
zhucc <1125517699@qq.com>
2017-08-19 11:32:57 +0800

    update

---
+++ ui-web-report/src/locate/index.js
@@ -0,0 +1,30 @@
+import elementId from 'element-ui/lib/locale/lang/id'
+import businessBasicId from "./future_basic_id"
+import businessCrmId from "./future_crm_id"
+import tableDataId from "./table_data_id"
+
+import elementZhCn from 'element-ui/lib/locale/lang/zh-CN'
+import businessBasicZhCn from "./future_basic_zh-CN"
+import businessCrmZhCn from "./future_crm_zh-CN"
+import tableDataZhCn from "./table_data_zh-CN"
+
+var idArray = [elementId,businessBasicId,businessCrmId,tableDataId];
+var zhCnArray = [elementZhCn,businessBasicZhCn,businessCrmZhCn,tableDataZhCn];
+
+var id = merge(idArray);
+var zhCn = merge(zhCnArray);
+
+function  merge(jsonArray) {
+  var resultJsonObject = {};
+  for (var index in jsonArray) {
+    for (var attr in jsonArray[index]) {
+      resultJsonObject[attr] = jsonArray[index][attr];
+    }
+  }
+  return resultJsonObject;
+}
+
+export default {
+  id:id,
+  zhCn:zhCn
+}
